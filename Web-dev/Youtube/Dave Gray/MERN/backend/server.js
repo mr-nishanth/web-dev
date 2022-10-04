dde6f1346acd224626d5d5d0de659433fa390911
mr-nishanth <innocentnishanth@gmail.com>
2022-10-04 17:56:03 +0530

    cors added

--- Web-dev/Youtube/Dave Gray/MERN/backend/server.js
+++ Web-dev/Youtube/Dave Gray/MERN/backend/server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {logger} = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const corsOptions = require("./config/corsOptions")
 const PORT = process.env.PORT || 3500
 
-
-
 // =========== Middleware ===========
-app.use("/",express.static(path.join(__dirname, 'public')))
-
+app.use(logger)
+// app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
+app.use(cookieParser())
 
+
+// Serve CSS File
+app.use("/",express.static(path.join(__dirname, 'public')))
 
 // =========== Routes ===========
 app.use("/",require("./routes/root"))
 
-app.all("*",(req,res,next)=>{
+app.all("*",(req,res)=>{
     res.status(404)
     if (req.accepts("html")){
         res.sendFile(path.join(__dirname,"views","404.html"))
@@ -24,6 +32,9 @@ app.all("*",(req,res,next)=>{
         res.type("txt").send("404 Not Found")
     }
 })
+
+// =========== Middleware ===========
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
