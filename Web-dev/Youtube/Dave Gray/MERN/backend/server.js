cdc69017fcb2cad14964b47c89f2d85cd3b171d4
mr-nishanth <innocentnishanth@gmail.com>
2022-10-04 18:55:41 +0530

    mongoose setup completed

--- Web-dev/Youtube/Dave Gray/MERN/backend/server.js
+++ Web-dev/Youtube/Dave Gray/MERN/backend/server.js
@@ -1,13 +1,24 @@
 require("dotenv").config()
+
 const express = require('express');
 const app = express();
 const path = require('path');
-const {logger} = require("./middleware/logger")
+const PORT = process.env.PORT || 3500
+
+const { logger, logEvents } = require("./middleware/logger")
 const errorHandler = require("./middleware/errorHandler")
+
 const cookieParser = require("cookie-parser")
+
 const cors = require("cors")
 const corsOptions = require("./config/corsOptions")
-const PORT = process.env.PORT || 3500
+
+const connectDB = require("./config/dbConn")
+const mongoose = require("mongoose");
+
+// Database setup
+connectDB()
+
 
 // =========== Middleware ===========
 app.use(logger)
@@ -18,18 +29,18 @@ app.use(cookieParser())
 
 
 // Serve CSS File
-app.use("/",express.static(path.join(__dirname, 'public')))
+app.use("/", express.static(path.join(__dirname, 'public')))
 
 // =========== Routes ===========
-app.use("/",require("./routes/root"))
+app.use("/", require("./routes/root"))
 
-app.all("*",(req,res)=>{
+app.all("*", (req, res) => {
     res.status(404)
-    if (req.accepts("html")){
-        res.sendFile(path.join(__dirname,"views","404.html"))
-    } else if (req.accepts("json")){
-        res.json({message:"404 Not Found"})
-    }else{
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"))
+    } else if (req.accepts("json")) {
+        res.json({ message: "404 Not Found" })
+    } else {
         res.type("txt").send("404 Not Found")
     }
 })
@@ -37,6 +48,15 @@ app.all("*",(req,res)=>{
 // =========== Middleware ===========
 app.use(errorHandler)
 
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB")
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+})
+
+mongoose.connection.on("error", (err) => {
+     console.log(err)
+     message=`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`
+     logEvents(message,"mongoErrLog.log") 
 })
