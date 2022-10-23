4f3fa363cfba2ebaf44021fbb57999655aefcfc8
mr-nishanth <innocentnishanth@gmail.com>
2022-10-23 11:56:53 +0530

    result component completed

--- College/Qutrix/RapidFireRound/client/src/components/Result.jsx
+++ College/Qutrix/RapidFireRound/client/src/components/Result.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
-
+import "../styles/Result.css"
+import { Link } from "react-router-dom"
+import ResultTable from './ResultTable'
 export default function Result() {
+    const onRestart = () => {
+        console.log("Restart")
+    }
     return (
-        <div>Result</div>
+        <div className='container'>
+            <h1 className='title text-light'>Rapid Fire Round</h1>
+
+            <div className="result flex-center">
+                <div className="flex">
+                    <span>Username</span>
+                    <span className='bold'>Nishanth</span>
+                </div>
+                <div className="flex">
+                    <span>Total Quiz Point : </span>
+                    <span className="bold">50</span>
+                </div>
+                <div className="flex">
+                    <span>Total Question : </span>
+                    <span className="bold">05</span>
+                </div>
+                <div className="flex">
+                    <span>Total Attempt : </span>
+                    <span className="bold">03</span>
+                </div>
+                <div className="flex">
+                    <span>Total Earn Point : </span>
+                    <span className="bold">30</span>
+                </div>
+                <div className="flex">
+                    <span>Quiz Result : </span>
+                    <span className="bold">Selected</span>
+                </div>
+            </div>
+
+            <div className="start">
+                <Link to={'/'} className="btn" onClick={onRestart}>Restart</Link>
+            </div>
+            <div className="container">
+                {/* Result Table */}
+                <ResultTable />
+            </div>
+        </div>
     )
 }
---
+++ College/Qutrix/RapidFireRound/client/src/components/ResultTable.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            <table>
+                <thead className='table-header'>
+                    <tr className="table-row">
+                        <td>Name</td>
+                        <td>Attempt</td>
+                        <td>Earn Points</td>
+                        <td>Result</td>
+                    </tr>
+                </thead>
+                <tbody className='table-row'>
+                    <tr className='table-body'>
+                        <td>Nishanth</td>
+                        <td>03</td>
+                        <td>20</td>
+                        <td>Select</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
