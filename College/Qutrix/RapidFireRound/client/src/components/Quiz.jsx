f750e530c4dd1c23f1cb3dd5d8c24fe042dae738
mr-nishanth <innocentnishanth@gmail.com>
2022-10-23 08:58:04 +0530

    button created in Quiz Component

--- College/Qutrix/RapidFireRound/client/src/components/Quiz.jsx
+++ College/Qutrix/RapidFireRound/client/src/components/Quiz.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export default function Quiz() {
+    // Next button event handler
+    const onNext = () => {
+        console.log("Next")
+    }
+    // Previous button event handler
+    const onPrev = () => {
+        console.log("Previous")
+    }
     return (
-        <div>Quiz</div>
+        <div className='container'>
+            <h1 className='title text-light'>Rapid Fire Round</h1>
+
+            {/* display question */}
+
+            {/* Button */}
+            <div className="grid">
+                <button onClick={onPrev} className='btn prev'>Previous</button>
+                <button onClick={onNext} className='btn next'>Next</button>
+            </div>
+        </div>
     )
 }
