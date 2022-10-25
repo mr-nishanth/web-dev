0a79cbc131af77d6fcd6aa3770498aac265ac367
mr-nishanth <innocentnishanth@gmail.com>
2022-10-25 10:52:04 +0530

    create anchor alternative to Link

--- Web-dev/Youtube/Daily Tution/react-router-v6/src/App.js
+++ Web-dev/Youtube/Daily Tution/react-router-v6/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-
+import { Link } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="container mx-auto">
+      <nav className='bg-gray-100 flex gap-4'>
+        {/* <a href="/invoice"><span>Invoice</span></a> */}
+        {/* <a href="/expenses"><span>Expenses</span></a> */}
+
+        <Link to="/invoice"><span>Invoice</span></Link>
+        <Link to="/expenses"><span>Expenses</span></Link>
+      </nav>
     </div>
   );
 }
