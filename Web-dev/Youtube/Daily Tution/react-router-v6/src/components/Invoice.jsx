2d361f6e8c5100ee1b0e8e1655569c8076301190
mr-nishanth <innocentnishanth@gmail.com>
2022-10-25 11:34:10 +0530

    List invoices

--- Web-dev/Youtube/Daily Tution/react-router-v6/src/components/Invoice.jsx
+++ Web-dev/Youtube/Daily Tution/react-router-v6/src/components/Invoice.jsx
@@ -1,10 +1,26 @@
-
+import { Link } from "react-router-dom"
+import { getInvoices } from "./data"
 
 const Invoice = () => {
+    let invoices = getInvoices()
     return (
-        <div className="text-center">
-            <h1>Invoice</h1>
-        </div>
+        <main className="text-center">
+            <div className="flex flex-col gap-3">
+                {
+                    invoices.map((invoice) => {
+                        return (
+                            <Link
+                                className="bg-gray-200"
+                                to={`/invoices/${invoice.number}`}
+                                key={invoice.number}
+                            >
+                                {invoice.name}
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+        </main>
     )
 }
 
---
+++ Web-dev/Youtube/Daily Tution/react-router-v6/src/components/data.js
@@ -0,0 +1,36 @@
+let invoices = [
+    {
+        name: "Black",
+        number: 1995,
+        amount: "$10,800",
+        due: "12/05/1995"
+    },
+    {
+        name: "Pearl",
+        number: 1999,
+        amount: "$12,000",
+        due: "12/05/1999"
+    },
+    {
+        name: "Kumar",
+        number: 2005,
+        amount: "$5,800",
+        due: "12/05/2005"
+    },
+    {
+        name: "Vijay",
+        number: 1980,
+        amount: "$8,800",
+        due: "12/05/1980"
+    },
+    {
+        name: "Surya",
+        number: 2000,
+        amount: "$15,800",
+        due: "12/05/2000"
+    }
+]
+
+export function getInvoices() {
+    return invoices;
+}
