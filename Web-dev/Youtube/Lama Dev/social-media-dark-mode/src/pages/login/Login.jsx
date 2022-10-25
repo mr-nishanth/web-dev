2f379fb4817e5d1fc19dd12b3d2125d876c1d343
mr-nishanth <innocentnishanth@gmail.com>
2022-10-25 10:31:48 +0530

    login page completed

--- Web-dev/Youtube/Lama Dev/social-media-dark-mode/src/pages/login/Login.jsx
+++ Web-dev/Youtube/Lama Dev/social-media-dark-mode/src/pages/login/Login.jsx
@@ -2,7 +2,24 @@ import "./login.scss"
 
 const Login = () => {
     return (
-        <div className="login">Login</div>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Black Media</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum placeat repellat soluta inventore? Assumenda hic dolore aliquam doloremque deserunt doloribus!</p>
+                    <span>Don't you have an account ?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form >
+                        <input type="text" name="" id="" placeholder="Username" />
+                        <input type="password" name="" id="" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
