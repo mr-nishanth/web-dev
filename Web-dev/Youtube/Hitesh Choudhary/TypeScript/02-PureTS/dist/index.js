d403be076b05576ef50d8dbf650ae29e2464e19c
mr-nishanth <innocentnishanth@gmail.com>
2022-11-12 10:55:34 +0530

    class getter and setter

--- Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/dist/index.js
+++ Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
 class User {
     constructor(email, name) {
+        //#name: string (# is coming form JS world , private is coming form TS world) 
         // city: string = ""
+        // readonly city: string = "Erode"
         this.city = "Erode";
         this.email = email;
         this.name = name;
@@ -10,3 +12,40 @@ class User {
 // const nis = new User(email:"n@n.com",name:"nisha")
 const nis = new User("n@n.com", "nisha");
 nis.city;
+//  Another Methods for creating class
+class UserTS {
+    constructor(email, name, userID) {
+        this.email = email;
+        this.name = name;
+        this.userID = userID;
+        this.city = "Erode";
+    }
+}
+//  getters and setters
+class UserGS {
+    constructor(email, name, userID) {
+        this.email = email;
+        this.name = name;
+        this.userID = userID;
+        this._courseCount = 1;
+        this.city = "Erode";
+    }
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    // getter
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    // setter
+    get courseCount() {
+        return this._courseCount;
+    }
+    // In set annotation does't have any return type
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
--- Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/index.ts
+++ Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/index.ts
@@ -1,9 +1,16 @@
 class User {
-    email: string
-    name: string
+    // default public
+    public email: string
+
+    private name: string
+    //#name: string (# is coming form JS world , private is coming form TS world) 
+
     // city: string = ""
 
-    readonly city: string = "Erode"
+    // readonly city: string = "Erode"
+
+    private readonly city: string = "Erode"
+
     constructor(email: string, name: string) {
         this.email = email;
         this.name = name
@@ -15,3 +22,59 @@ class User {
 
 const nis = new User("n@n.com", "nisha")
 nis.city
+
+
+
+
+//  Another Methods for creating class
+class UserTS {
+    readonly city: string = "Erode"
+    constructor
+        (
+            public email: string,
+            public name: string,
+            private userID: number
+        ) {
+
+    }
+}
+
+
+//  getters and setters
+
+class UserGS {
+
+    private _courseCount = 1
+    readonly city: string = "Erode"
+    constructor
+        (
+            public email: string,
+            public name: string,
+            private userID: number
+        ) {
+
+    }
+
+    private deleteToken(){
+        console.log("Token Deleted")
+    }
+
+    // getter
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+    // setter
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    // In set annotation does't have any return type
+    set courseCount(courseNum){
+        if (courseNum <= 1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
