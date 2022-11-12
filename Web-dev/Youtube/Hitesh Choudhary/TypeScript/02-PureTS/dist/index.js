96af39dbda358b4e559bb645fe56d069035c56d1
mr-nishanth <innocentnishanth@gmail.com>
2022-11-12 10:38:51 +0530

    intro class

--- Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/dist/index.js
+++ Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("TypeScript");
+class User {
+    constructor(email, name) {
+        // city: string = ""
+        this.city = "Erode";
+        this.email = email;
+        this.name = name;
+    }
+}
+// const nis = new User(email:"n@n.com",name:"nisha")
+const nis = new User("n@n.com", "nisha");
+nis.city;
