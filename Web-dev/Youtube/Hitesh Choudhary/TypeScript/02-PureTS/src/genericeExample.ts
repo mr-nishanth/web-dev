dff15c61a3eb71242a337b2225a29dc59aab78fe
mr-nishanth <innocentnishanth@gmail.com>
2022-11-16 18:40:45 +0530

    generic part 2

--- Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/genericeExample.ts
+++ Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/genericeExample.ts
@@ -39,4 +39,20 @@ function ownGenerics<Bootle>(val: Bootle): Bootle {
     return val
 }
 
+
+// Part 2
+// Generics Array[]
+function getSearchProducts<T>(products: T[]): T {
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+    // do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
 export { }
