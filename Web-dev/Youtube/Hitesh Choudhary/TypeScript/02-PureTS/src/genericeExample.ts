2e37c5255492b19eb89900f792aed204755d5731
mr-nishanth <innocentnishanth@gmail.com>
2022-11-16 18:53:01 +0530

    generics part 3

--- Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/genericeExample.ts
+++ Web-dev/Youtube/Hitesh Choudhary/TypeScript/02-PureTS/src/genericeExample.ts
@@ -55,4 +55,58 @@ const getMoreSearchProducts = <T>(products: T[]): T => {
     const myIndex = 4
     return products[myIndex]
 }
+
+
+// Part 3
+
+function anotherFunction<T, U>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+anotherFunction(3, "4")
+
+function anotherFunctionNumber<T, U extends number>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+anotherFunctionNumber(3, "4")
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function DBConnection<T, U extends Database>(v1: T, db: U): object {
+    return {
+        v1, db
+    }
+}
+DBConnection(6, {})
+
+interface Quiz {
+    name: string,
+    type: string,
+}
+
+interface Course {
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = []
+
+    addToCart(product: T) {
+        this.cart.push(product)
+    }
+}
+
 export { }
