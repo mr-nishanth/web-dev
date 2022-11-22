801d15119fbd525a0f496389b94ef1a138b6839d
mr-nishanth <innocentnishanth@gmail.com>
2022-11-22 11:09:55 +0530

    productCard

---
+++ Web-dev/Youtube/Tamil Skillhub/ecommerce-redux/src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function ProductCard(props) {
+    const navigate = useNavigate()
+    return (
+        <div className='card m-2 cursor-pointer' style={{ width: 300 }} role="button" onClick={() => navigate(`/product/${props.id}`)}>
+            <div className='mt-2'>
+                <img src={props.thumbnail} alt={props.title} height={150} width={180} className="border-radius-9" />
+            </div>
+            <div className='card-body'>
+                <h5 className='card-title'>{props.title}</h5>
+                <h6 className='card-text'>Price : ${props.price}</h6>
+                <h6 className='card-text'>Discount : {props.discountPercentage}%</h6>
+                <h6 className='card-text'>Rating : {props.rating}</h6>
+
+                <div>
+                    {props.stock > 40 ? <button className='btn btn-success'>Available</button> : <button className='btn btn-outline-danger'>Out of Stack</button>}
+                </div>
+
+            </div>
+        </div>
+    )
+}
