cc48422467d6b8294f07f4dd6cdb8ae6da642533
mr-nishanth <innocentnishanth@gmail.com>
2022-11-22 11:09:02 +0530

    home and card navigated setup

--- Web-dev/Youtube/Tamil Skillhub/ecommerce-redux/src/components/Header.jsx
+++ Web-dev/Youtube/Tamil Skillhub/ecommerce-redux/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Header() {
+    const navigate = useNavigate()
     return (
         <header className='bg-secondary p-3'>
             <h3 className='pb-3'>BlackShop</h3>
             <div className='row justify-content-center '>
                 <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex gap-3'>
-                    <button className='btn btn-success'>Home</button>
+                    <button className='btn btn-success' onClick={() => navigate("/")}>Home</button>
                     <input type="search" className='form-control' placeholder='Search product' />
-                    <button className='btn btn-success'>Cart</button>
+                    <button className='btn btn-success' onClick={() => navigate("/card")} > Cart</button>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
