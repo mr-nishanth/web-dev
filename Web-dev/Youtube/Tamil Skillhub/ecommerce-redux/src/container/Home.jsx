5c9b658e479e77577d76e3be4099d1c8b016b6be
mr-nishanth <innocentnishanth@gmail.com>
2022-11-22 11:08:27 +0530

    single product

--- Web-dev/Youtube/Tamil Skillhub/ecommerce-redux/src/container/Home.jsx
+++ Web-dev/Youtube/Tamil Skillhub/ecommerce-redux/src/container/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from '../components/Header'
+import Dashboard from './Dashboard'
+import Product from './Product'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/product/:id' element={<Product />} />
+      </Routes>
+    </>
   )
 }
