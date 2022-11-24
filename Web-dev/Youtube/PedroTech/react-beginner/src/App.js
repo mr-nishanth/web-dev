edd70ba9a9549365c3c314a665668d945a1de46d
mr-nishanth <innocentnishanth@gmail.com>
2022-11-24 11:45:35 +0530

    add and delete function added using useState hook

--- Web-dev/Youtube/PedroTech/react-beginner/src/App.js
+++ Web-dev/Youtube/PedroTech/react-beginner/src/App.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState("")
+  const handleInput = (e) => {
+    setNewTask(e.target.value)
+  }
+  const handleClick = (e) => {
+    let task = {
+      id: new Date().getTime(),
+      name: newTask
+    }
+    setTasks([...tasks, task])
+    console.log(task)
+  }
+  const handleDelete = (taskName) => {
+    // const deletedTodo = tasks.filter((task) => task === taskName ? false : true)
+    const deletedTodo = tasks.filter((task) => task.id !== taskName.id)
+    setTasks(deletedTodo)
+
+  }
   return (
     <div className="App">
-      <h1>Nishanth</h1>
+      <div className='addTask'>
+        <input type="text" onChange={handleInput} />
+        <button className='' onClick={handleClick}>Add Task</button>
+      </div>
+      <div className="list">
+
+        {
+          tasks.map((task) => (
+            <>
+              <div className='task' key={task.id}>
+                <h1 >{task.name}</h1>
+                <button onClick={() => handleDelete(task)}>X</button>
+              </div>
+            </>
+          ))
+        }
+
+      </div>
     </div>
   );
 }
