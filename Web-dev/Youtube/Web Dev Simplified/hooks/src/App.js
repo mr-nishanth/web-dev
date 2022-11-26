94eab106833e99837f222c92e50d71d4c3fc4add
mr-nishanth <innocentnishanth@gmail.com>
2022-11-26 06:48:07 +0530

    old code removed

--- Web-dev/Youtube/Web Dev Simplified/hooks/src/App.js
+++ Web-dev/Youtube/Web Dev Simplified/hooks/src/App.js
@@ -1,21 +1,11 @@
 
 import './App.css';
-import { Link, Outlet } from "react-router-dom"
+import Counter from './hooks/01-MustKowHooks/useStateHook/Counter';
+
 function App() {
   return (
-    <div className="container mx-auto">
-      <nav className='bg-gray-100 flex gap-4'>
-        {/* <a href="/invoice"><span>Invoice</span></a> */}
-        {/* <a href="/expenses"><span>Expenses</span></a> */}
-
-        <Link to="/invoice" >
-          <span>Invoice</span>
-        </Link>
-        <Link to="/expenses" >
-          <span>Expenses</span>
-        </Link>
-      </nav>
-      <Outlet />
+    <div className="App">
+      <Counter />
     </div>
   );
 }
