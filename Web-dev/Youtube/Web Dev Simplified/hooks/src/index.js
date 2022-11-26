67891dd954dc3be557cc838e6c258f26708cec9e
mr-nishanth <innocentnishanth@gmail.com>
2022-11-26 06:48:19 +0530

    old code removed

--- Web-dev/Youtube/Web Dev Simplified/hooks/src/index.js
+++ Web-dev/Youtube/Web Dev Simplified/hooks/src/index.js
@@ -3,33 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Invoice from './components/Invoice';
-import Expenses from './components/Expenses';
-import Bills from './components/Bills';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} >
-        <Route path='/invoice' element={<Invoice />} >
-
-          <Route path=':invoiceid' element={<Bills />}></Route>
-
-        </Route>
-        <Route path='/expenses' element={<Expenses />} />
-
-        {/* No matching route */}
-        <Route path='*' element={
-          <main className='text-center'>
-            <p className='text-3xl'>
-              There's nothing here!
-            </p>
-          </main>
-        } />
-      </Route>
-
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
