52d5a413f862799c663ba9c58970f44264813253
mr-nishanth <innocentnishanth@gmail.com>
2022-12-01 17:24:10 +0530

    Incomplete @ 34M

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react"
+import MovieListing from "../MovieListing/MovieListing"
 import "./Home.scss"
+import movieApi from "../../common/apis/movieApi"
+import { APIKey } from "../../common/apis/MovieApiKey"
 
-export default function Home() {
+
+const Home = () => {
+
+    useEffect(() => {
+        const movieText = "Harry"
+        const fetchMovie = async () => {
+
+            const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`).catch(err => console.log(`Error : ${err}`))
+            console.log(`The response was: `, response)
+        }
+
+        fetchMovie()
+    }, [])
+
     return (
-        <div>Home</div>
+        <>
+            <div className="banner-img">
+
+            </div>
+            <MovieListing />
+        </>
     )
 }
+
+export default Home
