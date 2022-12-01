ca73f2cac6a6e00b7ed5df53635a855a727f1a11
mr-nishanth <innocentnishanth@gmail.com>
2022-12-01 17:03:11 +0530

    header completed

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import "./Header.scss"
 
-export default function Header() {
+import React from 'react'
+import { Link } from "react-router-dom"
+import User from "../../images/user.png"
+function Header() {
     return (
-        <div>Header</div>
+        <header className="header">
+            <Link to="/">
+                <div className="logo">Movie App</div>
+            </Link>
+            <div className="user-image">
+                <img src={User} alt="user" />
+            </div>
+        </header>
     )
 }
+
+export default Header
