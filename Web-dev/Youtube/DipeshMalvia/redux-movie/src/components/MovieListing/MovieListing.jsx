0cd549f5069ac2a44374e5a21f70f06fb47d87e8
mr-nishanth <innocentnishanth@gmail.com>
2022-12-03 07:29:39 +0530

    MovieList

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieListing/MovieListing.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieListing/MovieListing.jsx
@@ -1,8 +1,33 @@
+import { useSelector } from "react-redux"
+import { getAllMovies } from "../../features/movies/movieSlice"
+import { MovieCard } from "../MovieCard/MovieCard"
 import "./MovieListing.scss"
 
 function MovieListing() {
+    const movies = useSelector(getAllMovies)
+    console.log(movies)
+    let renderMovies = "";
+
+    renderMovies = movies.Response === "True" ? (
+        movies.Search.map(movie => {
+            return (
+                <MovieCard key={movie.imdbID} data={movie} />
+            )
+        })
+    ) : (
+        <div className="movies-error"><h3>{movies.Error}</h3></div >
+    )
+
+
     return (
-        <div>MovieListing</div>
+        <div className="movie-wrapper">
+            <div className="movie-list">
+                <h2>Movies</h2>
+                <div className="movie-container">
+                    {renderMovies}
+                </div>
+            </div>
+        </div>
     )
 }
 
