c69051a222d30e87c0cf345408305fd1675ff6a1
mr-nishanth <innocentnishanth@gmail.com>
2022-12-03 07:29:29 +0530

    Home

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
@@ -3,16 +3,21 @@ import MovieListing from "../MovieListing/MovieListing"
 import "./Home.scss"
 import movieApi from "../../common/apis/movieApi"
 import { APIKey } from "../../common/apis/MovieApiKey"
+import { useDispatch } from "react-redux"
+import { addMovies } from "../../features/movies/movieSlice"
 
 
 const Home = () => {
-
+    const movieText = "Harry"
+    const dispatch = useDispatch()
     useEffect(() => {
-        const movieText = "Harry"
         const fetchMovie = async () => {
 
-            const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`).catch(err => console.log(`Error : ${err}`))
-            console.log(`The response was: `, response)
+            const res = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+                .catch(err => console.log(`Error : ${err}`))
+
+            // console.log(`The result was: `, res)
+            dispatch(addMovies(res.data))
         }
 
         fetchMovie()
