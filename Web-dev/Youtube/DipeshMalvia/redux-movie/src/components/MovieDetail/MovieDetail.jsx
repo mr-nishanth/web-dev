06bc82e9de68345ac7e8a71f47c673aecfce1ada
mr-nishanth <innocentnishanth@gmail.com>
2022-12-09 20:19:01 +0530

    Loading text added

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieDetail/MovieDetail.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieDetail/MovieDetail.jsx
@@ -20,45 +20,49 @@ function MovieDetail() {
 
     return (
         <div className="movie-section">
-            <div className="section-left">
-                <div className="movie-title">
-                    {data.Title}
-                </div>
-                <div className="movie-rating">
-                    <span>IMDB Rating <em className="fa fa-star"> {data.imdbRating}</em></span>
-                    <span>IMDB Votes <em className="fa fa-thumbs-up"> {data.imdbVotes}</em></span>
-                    <span>Runtime <em className="fa fa-film"> {data.Runtime}</em></span>
-                    <span>Year <em className="fa fa-calendar"> {data.Year}</em></span>
-                </div>
-                <div className="movie-plot">
-                    {data.Plot}
-                </div>
-                <div className="movie-info">
-                    <div>
-                        <span>Director</span>
-                        <span>{data.Director}</span>
+            {Object.keys(data).length === 0 ? (<div>Loading...</div>) : (
+                <>
+                    <div className="section-left">
+                        <div className="movie-title">
+                            {data.Title}
+                        </div>
+                        <div className="movie-rating">
+                            <span>IMDB Rating <em className="fa fa-star"> {data.imdbRating}</em></span>
+                            <span>IMDB Votes <em className="fa fa-thumbs-up"> {data.imdbVotes}</em></span>
+                            <span>Runtime <em className="fa fa-film"> {data.Runtime}</em></span>
+                            <span>Year <em className="fa fa-calendar"> {data.Year}</em></span>
+                        </div>
+                        <div className="movie-plot">
+                            {data.Plot}
+                        </div>
+                        <div className="movie-info">
+                            <div>
+                                <span>Director</span>
+                                <span>{data.Director}</span>
+                            </div>
+                            <div>
+                                <span>Stars</span>
+                                <span>{data.Actors}</span>
+                            </div>
+                            <div>
+                                <span>Genres</span>
+                                <span>{data.Genre}</span>
+                            </div>
+                            <div>
+                                <span>Languages</span>
+                                <span>{data.Language}</span>
+                            </div>
+                            <div>
+                                <span>Awards</span>
+                                <span>{data.Awards}</span>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <span>Stars</span>
-                        <span>{data.Actors}</span>
+                    <div className="section-right">
+                        <img src={data.Poster} alt={data.Title} />
                     </div>
-                    <div>
-                        <span>Genres</span>
-                        <span>{data.Genre}</span>
-                    </div>
-                    <div>
-                        <span>Languages</span>
-                        <span>{data.Language}</span>
-                    </div>
-                    <div>
-                        <span>Awards</span>
-                        <span>{data.Awards}</span>
-                    </div>
-                </div>
-            </div>
-            <div className="section-right">
-                <img src={data.Poster} alt={data.Title} />
-            </div>
+                </>
+            )}
         </div>
     )
 }
