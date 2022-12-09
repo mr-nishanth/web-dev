ba82a5ba4a7e230e01302b26b30bd61ae107aa77
mr-nishanth <innocentnishanth@gmail.com>
2022-12-09 20:08:08 +0530

    movieDetails Page

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieDetail/MovieDetail.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/MovieDetail/MovieDetail.jsx
@@ -16,7 +16,47 @@ function MovieDetail() {
     }, [dispatch, imdbID])
 
     return (
-        
+        <div className="movie-section">
+            <div className="section-left">
+                <div className="movie-title">
+                    {data.Title}
+                </div>
+                <div className="movie-rating">
+                    <span>IMDB Rating <em className="fa fa-star"> {data.imdbRating}</em></span>
+                    <span>IMDB Votes <em className="fa fa-thumbs-up"> {data.imdbVotes}</em></span>
+                    <span>Runtime <em className="fa fa-film"> {data.Runtime}</em></span>
+                    <span>Year <em className="fa fa-calendar"> {data.Year}</em></span>
+                </div>
+                <div className="movie-plot">
+                    {data.Plot}
+                </div>
+                <div className="movie-info">
+                    <div>
+                        <span>Director</span>
+                        <span>{data.Director}</span>
+                    </div>
+                    <div>
+                        <span>Stars</span>
+                        <span>{data.Actors}</span>
+                    </div>
+                    <div>
+                        <span>Genres</span>
+                        <span>{data.Genre}</span>
+                    </div>
+                    <div>
+                        <span>Languages</span>
+                        <span>{data.Language}</span>
+                    </div>
+                    <div>
+                        <span>Awards</span>
+                        <span>{data.Awards}</span>
+                    </div>
+                </div>
+            </div>
+            <div className="section-right">
+                <img src={data.Poster} alt={data.Title} />
+            </div>
+        </div>
     )
 }
 
