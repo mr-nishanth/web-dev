b5700b0312cf3c61ee44b36e161c2bce4204dc70
mr-nishanth <innocentnishanth@gmail.com>
2022-12-09 21:01:22 +0530

    empty term searching stopped using return

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     const handleSubmit = (e) => {
         e.preventDefault();
         // console.log(term)
-        if (term === "") alert("Please enter search term")
+        if (term === "") return alert("Please enter search term")
         dispatch(fetchAsyncMovies(term))
         dispatch(fetchAsyncShows(term))
         setTerm("")
