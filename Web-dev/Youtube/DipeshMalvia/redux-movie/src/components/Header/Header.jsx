5dbed015dd912a36dd22e9f9aa31d11d733089fa
mr-nishanth <innocentnishanth@gmail.com>
2022-12-09 20:58:22 +0530

    search bar added

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Header/Header.jsx
@@ -1,14 +1,37 @@
 import "./Header.scss"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import User from "../../images/user.png"
+import { useDispatch } from "react-redux"
+import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice"
 function Header() {
+    const [term, setTerm] = useState("")
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(term)
+
+        dispatch(fetchAsyncMovies(term))
+        dispatch(fetchAsyncShows(term))
+        setTerm("")
+    }
+    const handleChange = (e) => {
+        setTerm(e.target.value)
+    }
     return (
         <header className="header">
-            <Link to="/">
-                <div className="logo">Movie App</div>
-            </Link>
+            <div className="logo">
+                <Link to="/">
+                    Movie App
+                </Link>
+            </div>
+            <div className="search-bar">
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="text" value={term} placeholder="Search Movies or Shows" onChange={handleChange} />
+                    <button type="submit"><i className="fa fa-search"></i></button>
+                </form>
+            </div>
             <div className="user-image">
                 <img src={User} alt="user" />
             </div>
