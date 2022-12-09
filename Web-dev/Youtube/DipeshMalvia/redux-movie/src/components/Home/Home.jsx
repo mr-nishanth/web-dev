0858d9ebc62a77901c1ff0b4d22c3a42cead626e
mr-nishanth <innocentnishanth@gmail.com>
2022-12-09 20:58:44 +0530

    Default term added

--- Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
+++ Web-dev/Youtube/DipeshMalvia/redux-movie/src/components/Home/Home.jsx
@@ -6,9 +6,11 @@ import "./Home.scss"
 
 const Home = () => {
     const dispatch = useDispatch()
+    const movieText = "Harry"
+    const seriesText = "Friends"
     useEffect(() => {
-        dispatch(fetchAsyncMovies())
-        dispatch(fetchAsyncShows())
+        dispatch(fetchAsyncMovies(movieText))
+        dispatch(fetchAsyncShows(seriesText))
     }, [dispatch])
 
 
