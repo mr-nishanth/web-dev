87512c0beff07ff0737185e242ee7117bad2e602
mr-nishanth <innocentnishanth@gmail.com>
2022-12-10 16:57:57 +0530

    space added

--- Web-dev/Youtube/Dave Gray/MERN/backend/models/User.js
+++ Web-dev/Youtube/Dave Gray/MERN/backend/models/User.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+    username: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    roles:[
+    roles: [
         {
-            type:String,
-            default:"Employee",
+            type: String,
+            default: "Employee",
         },
     ],
     active:
-        {
-            type:Boolean,
-            default:true,
-        },
-    
+    {
+        type: Boolean,
+        default: true,
+    },
+
 })
 
 // why roles have array , because they have multiple roles like employee , admin and manager
 
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema)
