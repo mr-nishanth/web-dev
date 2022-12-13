4dfa18943653728e5d3e4ea63303f4d58a5b6c6c
mr-nishanth <innocentnishanth@gmail.com>
2022-12-13 21:00:52 +0530

    initial express app

--- Web-dev/Youtube/SanjeevThiyagarajan/nodeDocker/index.js
+++ Web-dev/Youtube/SanjeevThiyagarajan/nodeDocker/index.js
@@ -1,2 +1,9 @@
 require("dotenv").config()
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+})
+app.listen(PORT, () => console.log(`Application listening on http://localhost:${PORT}`));
