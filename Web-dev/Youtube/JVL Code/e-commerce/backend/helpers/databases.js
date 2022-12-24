a03cefe4e1352e09f09e6fe8f075e85979b77e7f
mr-nishanth <innocentnishanth@gmail.com>
2022-12-24 20:22:26 +0530

    database connection

---
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/helpers/databases.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
+const MONGO_URI = process.env.MONGODB_LOCAL
+const connectDB = async () => {
+    try {
+        const result = await mongoose.connect(MONGO_URI, {
+            // useNewUrlParams: true,
+            useUnifiedTopology: true
+        })
+        // console.log(`MongoDB is connected to the host : ${result.connection.host}`)
+        console.log(`\n MongoDB is connected to the host : ${result.connection.host} \n`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+module.exports = connectDB
