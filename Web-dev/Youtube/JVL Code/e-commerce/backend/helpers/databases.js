9e0f1707ebc39a3cdbea5668439117a7fdd088a6
mr-nishanth <innocentnishanth@gmail.com>
2022-12-25 13:38:56 +0530

    unhandledRejection

--- Web-dev/Youtube/JVL Code/e-commerce/backend/helpers/databases.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/helpers/databases.js
@@ -3,16 +3,15 @@ mongoose.set('strictQuery', true);
 const connectDB = async () => {
     const MONGO_URI = process.env.MONGODB_LOCAL
     console.log(`🥭 Database URI : ${MONGO_URI}`)
-    try {
-        const result = await mongoose.connect(MONGO_URI, {
-            // useNewUrlParams: true,
-            useUnifiedTopology: true
-        })
+
+    const result = await mongoose.connect(MONGO_URI, {
+        // useNewUrlParams: true,
+        useUnifiedTopology: true
+    }).then((result) => {
         // console.log(`MongoDB is connected to the host : ${result.connection.host}`)
         console.log(`\n MongoDB is connected to the host : ${result.connection.host} \n`)
-    } catch (error) {
-        console.log(error.message)
-    }
+    })
+
 }
 
 module.exports = connectDB
