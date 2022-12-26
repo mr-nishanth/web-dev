c0e7396427b5a0698edd5e42b4d6a905eaa87bbb
mr-nishanth <innocentnishanth@gmail.com>
2022-12-26 12:53:27 +0530

    Initial Stripe setup

---
+++ Web-dev/Youtube/Web Dev Simplified/stripe/server/server.js
@@ -0,0 +1,22 @@
+const path = require("path")
+require("dotenv").config({ path: path.join(__dirname, "config", "config.env") })
+const express = require('express');
+const app = express();
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+const storeItems = new Map([
+    [
+        1, { priceCents: 10000, name: "Learn Stripe" }
+    ],
+    [
+        2, { priceCents: 20000, name: "Learn Node" }
+    ]
+])
+
+
+app.listen(process.env.PORT || 3500, () => {
+    console.log(`🚀🚀🚀 is ready to launch at ${process.env.PORT} port`)
+})
