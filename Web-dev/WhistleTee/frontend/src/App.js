d82e4679075fac10974c85700aaebbeeec8e777f
mr-nishanth <innocentnishanth@gmail.com>
2022-12-28 15:22:55 +0530

    todo and email

--- Web-dev/WhistleTee/frontend/src/App.js
+++ Web-dev/WhistleTee/frontend/src/App.js
@@ -1,37 +1,72 @@
-import Dashboard from "./Components/Dashboard"
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-// import DashboardPage from "./pages/Dashboard";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import AddStudent from "./Components/AddStudent"
-import UpdateUser from "./Components/UpdateUser"
-import Payment from "./Components/Payment"
-import "./App.css"
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
+import React, { useEffect, useState } from 'react'
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+  const [todoHeading, setTodoHeading] = useState("")
+  const [email, setEmail] = useState("")
+
+  // const handleChange = (e) => {
+  //   //   setUser((prev) => {
+  //   //     const addUser = { ...prev, [e.target.name]: e.target.value }
+  //   //     console.log(addUser)
+  //   //     return addUser;
+  //   // });
+
+  //   // setTodo((prev) => {
+  //   //   const newTodo = { ...prev, [e.target.name]: e.target.value }
+  //   //   return newTodo;
+  //   // })
+
+  //   setTodo(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  // }
+
+  // useEffect(() => {
+  //   console.log(todo)
+  // }, [todo])
+
+  // const addTodo = () => {
+
+  // }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // addTodo()
+    setTodos([...todos, { email: email, todoHeading: todoHeading }])
+
+  }
 
 
-function App() {
   return (
-    <>
-      <div className="main">
-        {/* <NavBar /> */}
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/create" element={<AddStudent />}></Route>
-            <Route path="/update/:id" element={<UpdateUser />}></Route>
-            <Route path="/payment/:id" element={<Payment />}></Route>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </Router>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <form action="" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder='Todo'
+          onChange={e => setTodoHeading(e.target.value)}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder='Email'
+          onChange={e => setEmail(e.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
 
+      <div style={{ marginTop: "20px" }}>
+        {todos.map((todo, index) => {
+          return (
+            <div key={crypto.randomUUID()}>
+              {todo.todoHeading}
+              {todo.email}
+            </div>
+          )
+        })}
       </div>
-    </>
 
+    </div>
   )
 }
-
-export default App
