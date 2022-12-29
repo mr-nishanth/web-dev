c686cfa6761f6db1059c35187e645a53a784965d
mr-nishanth <innocentnishanth@gmail.com>
2022-12-29 16:27:58 +0530

    initial Consumer and provider Controller

---
+++ Web-dev/WhistleTee/backend/controllers/consumer.controller.js
@@ -0,0 +1,4 @@
+exports.getAllConsumer = (req, res) => { }
+exports.createConsumer = (req, res) => { }
+exports.updateConsumer = (req, res) => { }
+exports.deleteConsumer = (req, res) => { }
---
+++ Web-dev/WhistleTee/backend/controllers/provider.controller.js
@@ -0,0 +1,4 @@
+exports.getAllProvider = (req, res) => { }
+exports.createProvider = (req, res) => { }
+exports.updateProvider = (req, res) => { }
+exports.deleteProvider = (req, res) => { }
