f8c0e48c9f8a353ae59b5bc4427f2814cbef1ba5
mr-nishanth <innocentnishanth@gmail.com>
2022-12-29 16:28:15 +0530

    Consumer and Provider routes

--- Web-dev/WhistleTee/backend/routes/consumer.routes.js
+++ Web-dev/WhistleTee/backend/routes/consumer.routes.js
@@ -1,3 +1,10 @@
+const { getAllConsumer, createConsumer, updateConsumer, deleteConsumer } = require("../controllers/consumer.controller");
+
 const consumerRouter = require("express").Router();
+
+consumerRouter.get("/", getAllConsumer)
+consumerRouter.post("/create", createConsumer)
+consumerRouter.put("/update", updateConsumer)
+consumerRouter.delete("/delete", deleteConsumer)
 
 module.exports = consumerRouter
--- Web-dev/WhistleTee/backend/routes/provider.routes.js
+++ Web-dev/WhistleTee/backend/routes/provider.routes.js
@@ -1,3 +1,11 @@
+const { getAllProvider, createProvider, updateProvider, deleteProvider } = require("../controllers/provider.controller");
+
+
 const providerRouter = require("express").Router();
+
+providerRouter.get("/", getAllProvider)
+providerRouter.post("/create", createProvider)
+providerRouter.put("/update", updateProvider)
+providerRouter.delete("/delete", deleteProvider)
 
 module.exports = providerRouter
