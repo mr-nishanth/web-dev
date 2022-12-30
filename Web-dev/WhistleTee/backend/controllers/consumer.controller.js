d99a070b13423871e031f42ff7a6b1b8f587a344
mr-nishanth <innocentnishanth@gmail.com>
2022-12-30 13:50:26 +0530

    consumer create completed

--- Web-dev/WhistleTee/backend/controllers/consumer.controller.js
+++ Web-dev/WhistleTee/backend/controllers/consumer.controller.js
@@ -1,31 +1,40 @@
 const Consumer = require("../model/consumer.model")
+const moment = require('moment');
 exports.getAllConsumer = async (req, res) => {
     try {
-        const allProvider = await Consumer.find().lean()
-        if (!allProvider) return res.status(404).json({ success: false, message: "Provider not found" })
-        return res.status(200).json({ success: true, allProvider })
+        const allConsumer = await Consumer.find().lean()
+        if (!allConsumer) return res.status(404).json({ success: false, message: "Provider not found" })
+        return res.status(200).json({ success: true, allConsumer })
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
     }
 }
 exports.createConsumer = async (req, res) => {
-    const { providerName, golfName, location, price, numberOfSlots, selectedDate } = req.body
-    if (!providerName || !golfName || !location || !price || !numberOfSlots || !selectedDate) {
+    const { consumerName, coordinates, email, mobileNumber, alertRadius, expireTime } = req.body
+    console.log(req.body)
+    if (!consumerName || !email || !coordinates || !mobileNumber || !alertRadius || !expireTime) {
         return res.status(400).json({ success: false, message: "All fields required" })
     }
+    const longCoordinates = parseFloat(coordinates.split(",")[0])
+    // console.log(longCoordinates)
+    const latCoordinates = parseFloat(coordinates.split(",")[1])
+    // console.log(latCoordinates)
+    const splitCoordinates = [longCoordinates, latCoordinates]
+    console.log(splitCoordinates)
     try {
-        const newProvider = await Provider.create(
+        const newConsumer = await Consumer.create(
             {
-                providerName,
-                golfName,
-                location: { coordinates: location },
-                price,
-                numberOfSlots,
-                selectedDate
+                consumerName,
+                email,
+                mobileNumber,
+                alertRadius: parseInt(alertRadius),
+                expireTime: moment().add(parseInt(expireTime), 'hours').toDate(),
+                location: { coordinates: splitCoordinates },
+
             }
         )
-        const provider = await newProvider.save()
-        return res.status(200).json({ success: true, provider })
+        const consumer = await newConsumer.save()
+        return res.status(200).json({ success: true, consumer, message: "Consumer whistle added successfully" })
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
     }
