4c4d4ca4a5fb1327174843e29f3c9e2c418312b9
mr-nishanth <innocentnishanth@gmail.com>
2022-12-30 13:50:37 +0530

    provider create completed

--- Web-dev/WhistleTee/backend/controllers/provider.controller.js
+++ Web-dev/WhistleTee/backend/controllers/provider.controller.js
@@ -1,4 +1,5 @@
 const Provider = require('../model/provider.models')
+const moment = require('moment')
 exports.getAllProvider = async (req, res) => {
     try {
         const allProvider = await Provider.find().lean()
@@ -9,23 +10,50 @@ exports.getAllProvider = async (req, res) => {
     }
 }
 exports.createProvider = async (req, res) => {
-    const { providerName, golfName, location, price, numberOfSlots, selectedDate } = req.body
-    if (!providerName || !golfName || !location || !price || !numberOfSlots || !selectedDate) {
+    const { providerName, golfName, coordinates, price, numberOfSlots, selectedDate } = req.body
+    if (!providerName || !golfName || !coordinates || !price || !numberOfSlots || !selectedDate) {
         return res.status(400).json({ success: false, message: "All fields required" })
     }
+    const longCoordinates = parseFloat(coordinates.split(",")[0])
+    // console.log(longCoordinates)
+    const latCoordinates = parseFloat(coordinates.split(",")[1])
+    // console.log(latCoordinates)
+    const splitCoordinates = [longCoordinates, latCoordinates]
+    // console.log(splitCoordinates)
+    // console.log("selectedDate", typeof selectedDate)
+    // console.log("selectedDate IST", new Date(selectedDate).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' }))
+    console.log(req.body);
+
+    // let dateConverter = new Date(selectedDate).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })
+    // console.log("dateConverter", typeof dateConverter)
+    // let dateConverter = moment(selectedDate).format()
+    // let dateConverter = moment(moment(selectedDate).format()).toDate()
+    let dateConverter = moment(moment(selectedDate).add(5, "hours").add(30, "minutes")).toDate()
+
+    // dateConverter = new Date(dateConverter)
+    console.log("dateConverter", dateConverter)
+    // console.log("dateConverter", new Date(dateConverter))
+    console.log("dateConverter TYPE", typeof dateConverter)
+    // console.log("dateConverter TYPE", typeof new Date(dateConverter))
+
+    // let nisha = Date.parse(dateConverter)
+    // console.log("Nisha", nisha)
+    // console.log("Nisha Type : ", typeof nisha)
+
     try {
         const newProvider = await Provider.create(
             {
                 providerName,
                 golfName,
-                location: { coordinates: location },
-                price,
-                numberOfSlots,
-                selectedDate: new Date(selectedDate.$d).toISOString()
+                location: { coordinates: splitCoordinates },
+                price: parseFloat(price),
+                numberOfSlots: parseInt(numberOfSlots),
+                // selectedDate: new Date(dateConverter)
+                selectedDate: dateConverter
             }
         )
         const provider = await newProvider.save()
-        return res.status(200).json({ success: true, provider })
+        return res.status(200).json({ success: true, provider, message: "Provider whistle added successfully" })
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
     }
