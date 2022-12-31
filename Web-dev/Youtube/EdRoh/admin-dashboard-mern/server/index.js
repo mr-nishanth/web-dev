8b3a3b46b75ce691daff55d05881a1541c70ea39
mr-nishanth <innocentnishanth@gmail.com>
2022-12-31 14:26:04 +0530

    Module not found bug fixed

--- Web-dev/Youtube/EdRoh/admin-dashboard-mern/server/index.js
+++ Web-dev/Youtube/EdRoh/admin-dashboard-mern/server/index.js
@@ -6,16 +6,18 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 
-import clientRoutes from "./routes/client.routes"
-import generalRoutes from "./routes/general.routes"
-import managementRoutes from "./routes/management.routes"
-import salesRoutes from "./routes/sales.routes"
+import clientRoutes from "./routes/client.routes.js"
+import generalRoutes from "./routes/general.routes.js";
+import managementRoutes from "./routes/management.routes.js";
+import salesRoutes from "./routes/sales.routes.js";
+
+
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 // ? CONFIGURATION
-dotenv.config()
+dotenv.config({ path: "./config/config.env" })
 app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
@@ -29,3 +31,21 @@ app.use("/client", clientRoutes)
 app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
+
+
+// MONGOOSE SETUP
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_LOCAL_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.listen(PORT, () => console.log(`\n 👉 🚀 is ready for lunch at http://localhost:${PORT} path \n`))
+}).catch(err => console.log(err.message))
+
+
+
+// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true })
+// mongoose.connection.on("connected", () => {
+//     console.log("Mongoose connected!")
+// }
+// mongoose.set("useCreateIndex", true)
+// mongoose.set("useFindAndModify", false)
+// mongoose.set("useUnifiedTopology", true)
+// mongoose.set("useFindAndModify", false)
--- Web-dev/Youtube/EdRoh/admin-dashboard-mern/server/routes/client.routes.js
+++ Web-dev/Youtube/EdRoh/admin-dashboard-mern/server/routes/client.routes.js
@@ -1,4 +1,16 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+// import {
+//   getProducts,
+//   getCustomers,
+//   getTransactions,
+//   getGeography,
+// } from "../controllers/client.js";
 
-export default router
+const router = express.Router();
+
+// router.get("/products", getProducts);
+// router.get("/customers", getCustomers);
+// router.get("/transactions", getTransactions);
+// router.get("/geography", getGeography);
+
+export default router;
