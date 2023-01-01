4ca050e779c41d289eff93f8a715438ef3a09c5f
mr-nishanth <innocentnishanth@gmail.com>
2023-01-01 11:59:40 +0530

    authentication complete using bcrypt js

---
+++ Web-dev/Youtube/TraversyMedia/MERN-Stack/server/controllers/user.controller.js
@@ -0,0 +1,77 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+// @desc Register a user
+// @route POST /api/users
+// @access Public
+export const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please provide all required fields");
+  }
+
+  //^  Check if the user is already registered
+  const user = await User.findOne({ email: email });
+  if (user) {
+    res.status(409);
+    throw new Error("User already exists");
+  }
+
+  //^  Generate a random password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //^  Create the user
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  await newUser.save();
+
+  if (newUser) {
+    return res.status(201).json({ _id: newUser._id });
+  } else {
+    res.status(500);
+    throw new Error("Invalid User Data");
+  }
+
+  //^  Send the token to the client
+});
+
+// @desc Login a user/Authenticate a user
+// @route POST /api/users/login
+// @access Public
+export const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please provide all required fields");
+  }
+
+  //^  Check if the user is registered
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // ^ Compare the user entered password with the user password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return res.status(200).json({ _id: user._id, name: user.name });
+  } else {
+    res.status(500);
+    throw new Error("Invalid User Data");
+  }
+});
+
+// @desc Get User Profile
+// @route GET /api/users/me
+// @access Private
+export const getMe = asyncHandler(async (req, res, next) => {
+  res.send("Me");
+});
