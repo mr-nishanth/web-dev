281bed494cdb49fb973698836348065890953063
mr-nishanth <innocentnishanth@gmail.com>
2023-01-01 18:53:50 +0530

    header comp added

--- Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/App.js
+++ Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div className="">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
