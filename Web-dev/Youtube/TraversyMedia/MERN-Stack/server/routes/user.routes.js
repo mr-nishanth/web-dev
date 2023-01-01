2fa244a08e5219793ba16401bb70557f00ef6943
mr-nishanth <innocentnishanth@gmail.com>
2023-01-01 12:42:17 +0530

    add get all user route

--- Web-dev/Youtube/TraversyMedia/MERN-Stack/server/routes/user.routes.js
+++ Web-dev/Youtube/TraversyMedia/MERN-Stack/server/routes/user.routes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import {
+  getAll,
   getMe,
   loginUser,
   registerUser,
 } from "../controllers/user.controller.js   ";
+import { protect } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", [protect, getMe]);
+router.get("/all", [getAll]);
 
 export default router;
