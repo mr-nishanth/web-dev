26e68f045752d59c56b86f6f85fd42e5b6b5bb2c
mr-nishanth <innocentnishanth@gmail.com>
2023-01-01 08:06:57 +0530

    flex between

--- Web-dev/Youtube/EdRoh/admin-dashboard-mern/client/src/scenes/layout/index.jsx
+++ Web-dev/Youtube/EdRoh/admin-dashboard-mern/client/src/scenes/layout/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Box , useMediaQuery } from '@mui/icons-material'
+import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Navbar from 'components/Navbar'
 const Layout = () => {
+  const [first, setFirst] = useState(second)
   return (
-    <div>Layout</div>
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
   )
 }
 
