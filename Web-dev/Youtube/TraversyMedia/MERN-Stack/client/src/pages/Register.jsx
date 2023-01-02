a571091407a5da8555a552e9c4f86ea4f4966ba9
mr-nishanth <innocentnishanth@gmail.com>
2023-01-02 11:44:21 +0530

    register comp...

--- Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/pages/Register.jsx
+++ Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -10,15 +15,52 @@ const Register = () => {
 
   const { name, email, password, cPassword } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const handleChange = (e) => {
     setFormData((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
     }));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (password !== cPassword) {
+      toast.error("Password is not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
+    }
   };
+
+  if (isLoading) {
+    return (
+      <>
+        <Spinner />
+      </>
+    );
+  }
+
   return (
     <>
       <section className="heading">
