1961a4457b23f7d1897601d6d1c1ac1a883929be
mr-nishanth <innocentnishanth@gmail.com>
2023-01-02 11:47:22 +0530

    auth service

--- Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/features/auth/authService.js
+++ Web-dev/Youtube/TraversyMedia/MERN-Stack/client/src/features/auth/authService.js
@@ -1,10 +1,20 @@
 import axios from "axios";
-
-const API_URL = "/api/users";
+axios.defaults.baseURL = `http://localhost:3500`;
+const API_REGISTER_URL = `/api/users/register`;
+const API_LOGIN_URL = `/api/users/login`;
 
 // Register a new user
 const register = async (userData) => {
-  const response = await axios.post(API_URL.userData);
+  const response = await axios.post(API_REGISTER_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+// login user
+const login = async (userData) => {
+  const response = await axios.post(API_LOGIN_URL, userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
@@ -12,8 +22,14 @@ const register = async (userData) => {
   return response.data;
 };
 
+// Logout
+const logout = () => {
+  localStorage.removeItem("user");
+};
 const authService = {
   register,
+  logout,
+  login,
 };
 
 export default authService;
