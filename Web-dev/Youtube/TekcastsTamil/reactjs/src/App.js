3c0333792cdf4cf8f245309ab188e324c72f654f
mr-nishanth <innocentnishanth@gmail.com>
2023-01-03 14:55:42 +0530

    API

--- Web-dev/Youtube/TekcastsTamil/reactjs/src/App.js
+++ Web-dev/Youtube/TekcastsTamil/reactjs/src/App.js
@@ -1,15 +1,17 @@
-import StateAndProps from "./session/02-session/StateAndProps";
+import StateAndProps from "./session/02-session - State and Props/StateAndProps";
 import StyledComp from "./session/03-session - Styled Components/StyledComp";
 import UseStateEx from "./session/04-session - useState/UseStateEx";
 import LifeCycle from "./session/05-session - LifeCycle Methods/LifeCycle";
+import UseEffectEx from "./session/06-session - useEffect/UseEffectEx";
 
 function App() {
   return (
     <>
-      {/* <StateAndProps></StateAndProps> */}
+      {/* <StateAndProps /> */}
       {/* <StyledComp /> */}
       {/* <UseStateEx /> */}
-      <LifeCycle />
+      {/* <LifeCycle /> */}
+      <UseEffectEx />
     </>
   );
 }
