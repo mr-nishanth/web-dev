546f170c52b310d1a7048cecf30d02d3ec92210f
mr-nishanth <innocentnishanth@gmail.com>
2023-01-03 14:55:03 +0530

    useEffect

---
+++ Web-dev/Youtube/TekcastsTamil/reactjs/src/session/06-session - useEffect/UseEffectEx.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import UseEffectBasic from "./UseEffectBasic";
+import UseEffectAPI from "./UseEffectAPI";
+const UseEffectEx = () => {
+  return (
+    <div>
+      {/* <UseEffectBasic /> */}
+      <UseEffectAPI />
+    </div>
+  );
+};
+export default UseEffectEx;
