192ce9dd30dbb1a9ce79e817467ee35eb8f2b41b
mr-nishanth <innocentnishanth@gmail.com>
2023-01-09 17:17:47 +0530

    header

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/components/Header.jsx
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/components/Header.jsx
@@ -1,25 +1,31 @@
 import {
   Badge,
+  Button,
   Card,
   Container,
   Dropdown,
   FormControl,
   Nav,
   Navbar,
+  NavbarBrand,
 } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CartState } from "../context/Context";
+import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 const Header = () => {
   const {
     state: { cart },
+    dispatch,
   } = CartState();
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
-        <Navbar.Brand>
+        <NavbarBrand>
           <Link to="/">Shopping Carts</Link>
-        </Navbar.Brand>
+        </NavbarBrand>
         <Navbar.Text className="search">
           <FormControl
             style={{ width: 500 }}
@@ -30,15 +36,58 @@ const Header = () => {
         </Navbar.Text>
 
         <Nav>
-          <Dropdown align={"end"}>
-            <Dropdown.Toggle variant="success">
+          <Dropdown align={"start"}>
+            <DropdownToggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
               {/* <Badge>{cart.length}</Badge> */}
               <Badge>{cart.length}</Badge>
-            </Dropdown.Toggle>
-            <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{ padding: 10 }}>Cart is Empty!!</span>
-            </Dropdown.Menu>
+            </DropdownToggle>
+            <DropdownMenu
+              style={{
+                minWidth: 370,
+                left: "10!important",
+                right: "0!important",
+              }}
+            >
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => {
+                    return (
+                      <span className="cartItem" key={prod.id}>
+                        <img
+                          src={prod.image}
+                          className="cartItemImg"
+                          alt={prod.name}
+                        />
+                        <div className="cartItemDetail">
+                          <span>{prod.name}</span>
+                          <span>$ {prod.price.split(".")[0]}</span>
+                        </div>
+                        <AiFillDelete
+                          fontSize="20px"
+                          style={{ cursor: "pointer" }}
+                          onClick={() =>
+                            dispatch({
+                              type: "REMOVE_FROM_CART",
+                              payload: prod,
+                            })
+                          }
+                        />
+                      </span>
+                    );
+                  })}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go to Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <span style={{ padding: 10 }}>Cart is Empty!!</span>
+                </>
+              )}
+            </DropdownMenu>
           </Dropdown>
         </Nav>
       </Container>
