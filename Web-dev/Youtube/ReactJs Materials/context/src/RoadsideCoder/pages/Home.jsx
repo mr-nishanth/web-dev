c8ea894fbc44ed2a39cc7f11384d4df1a05af18a
mr-nishanth <innocentnishanth@gmail.com>
2023-01-09 06:20:54 +0530

    Home pages

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Home.jsx
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Home.jsx
@@ -1,4 +1,22 @@
+import Filter from "../components/Filter";
+import SingleProduct from "../components/SingleProduct";
+import { CartState } from "../context/Context";
+import "./style.css";
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    state: { products },
+    dispatch,
+  } = CartState();
+  console.log(products);
+  return (
+    <div className="home">
+      <Filter />
+      <div className="productContainer">
+        {products.map((product) => {
+          return <SingleProduct product={product} key={product.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 export default Home;
