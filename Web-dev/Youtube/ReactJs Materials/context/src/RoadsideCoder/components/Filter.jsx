33c7cc1a5c9c1c23dc56e39ab36f1238a47c6d96
mr-nishanth <innocentnishanth@gmail.com>
2023-01-09 06:20:17 +0530

    filter comp...

---
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/components/Filter.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Filter = () => {
+  const [rate, setRate] = useState(2);
+  return (
+    <div className="filters">
+      <span className="title">Filter Products</span>
+      <span>
+        <Form.Check
+          inline
+          label="Ascending"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Descending"
+          name="group1"
+          type="radio"
+          id={`inline-2`}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Include Out of Stock"
+          name="group1"
+          type="checkbox"
+          id={`inline-3`}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Fast Delivery"
+          name="group1"
+          type="checkbox"
+          id={`inline-4`}
+        />
+      </span>
+      <span>
+        <label htmlFor="" style={{ paddingRight: 30 }}>
+          Rating
+        </label>
+        <Rating
+          rating={rate}
+          onClick={(index) => setRate(index + 1)}
+          style={{ cursor: "pointer" }}
+        />
+      </span>
+      <Button variant="light">Clear Filter</Button>
+    </div>
+  );
+};
+export default Filter;
