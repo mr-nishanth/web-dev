375044ead1ddaeb7091a3f9c9f00e1fb592ee4d6
mr-nishanth <innocentnishanth@gmail.com>
2023-01-09 06:45:11 +0530

    bug fix card to cart

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Context.js
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Context.js
@@ -1,19 +1,31 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { faker } from "@faker-js/faker";
+import { cardReducer } from "./Reducer";
 const Cart = createContext();
+
+faker.seed(99);
 const Context = ({ children }) => {
   // [...Array(20)] its create 20 undefine array
   const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.image.fashion(),
+    image: faker.image.food(640, 480, true),
     inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
     rating: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
   }));
   // console.log(products);
 
-  return <Cart.Provider>{children}</Cart.Provider>;
+  const [state, dispatch] = useReducer(cardReducer, {
+    products: products,
+    cart: [],
+  });
+
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 };
 export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
