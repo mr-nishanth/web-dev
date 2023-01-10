eef3fb2f9205774e5ca20b85e11b899aca0a9d4b
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 10:23:37 +0530

    Filters added

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
@@ -7,6 +7,38 @@ export const cardReducer = (state, action) => {
         ...state,
         cart: state.cart.filter((c) => c.id !== action.payload.id),
       };
+    case "CHANGE_CART_QTY":
+      return {
+        ...state,
+        cart: state.cart.filter((c) =>
+          c.id === action.payload.id ? (c.qty = action.payload.qty) : c.qty
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export const productReducer = (state, action) => {
+  switch (action.type) {
+    case "SORT_BY_PRICE":
+      return { ...state, sort: action.payload };
+    case "SORT_BY_STOCK":
+      return { ...state, byStock: !state.byStock };
+    case "SORT_BY_DELIVERY":
+      return { ...state, byFastDelivery: !state.byFastDelivery };
+    case "SORT_BY_RATING":
+      return { ...state, byRating: state.payload };
+    case "SORT_BY_SEARCH":
+      return { ...state, searchQuery: state.payload };
+    case "CLEAR_FILTERS":
+      return {
+        byStock: false,
+        byFastDelivery: false,
+        byRating: 0,
+        searchQuery: "",
+      };
+
     default:
       return state;
   }
