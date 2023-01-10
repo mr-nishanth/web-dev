9c9784f0959fd9fe678f0abeb0dcd7fbd93b6a27
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 14:03:04 +0530

    Product Reducer

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
@@ -29,7 +29,7 @@ export const productReducer = (state, action) => {
       return { ...state, byFastDelivery: !state.byFastDelivery };
     case "FILTER_BY_RATING":
       return { ...state, byRating: action.payload };
-    case "SORT_BY_SEARCH":
+    case "FILTER_BY_SEARCH":
       return { ...state, searchQuery: action.payload };
     case "CLEAR_FILTERS":
       return {
