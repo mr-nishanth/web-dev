023883e2d31cb1c0d098fc576c8a2e5ea9552eed
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 14:03:13 +0530

    Home

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Home.jsx
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Home.jsx
@@ -5,14 +5,45 @@ import "./style.css";
 const Home = () => {
   const {
     state: { products },
+    productState: { byStock, byFastDelivery, byRating, searchQuery, sort },
     dispatch,
   } = CartState();
-  console.log(products);
+  // console.log(products);
+  const transformProducts = () => {
+    let sortedProducts = products;
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        // a.price - b.price (Ascending): b.price - a.price (Descending)
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter((prod) => prod.rating >= byRating);
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
+
   return (
     <div className="home">
       <Filter />
       <div className="productContainer">
-        {products.map((product) => {
+        {transformProducts().map((product) => {
           return <SingleProduct product={product} key={product.id} />;
         })}
       </div>
