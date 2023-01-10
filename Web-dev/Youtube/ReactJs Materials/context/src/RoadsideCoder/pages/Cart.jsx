29cbc9cc6a9785976f9b3dcc144a0f171619b4da
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 10:47:33 +0530

    Cart

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Cart.jsx
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/pages/Cart.jsx
@@ -34,7 +34,19 @@ const Cart = () => {
                   <Rating rating={prod.rating} />
                 </Col>
                 <Col md={2}>
-                  <Form.Control as="select" value={prod.qty}>
+                  <Form.Control
+                    as="select"
+                    value={prod.qty}
+                    onChange={(e) =>
+                      dispatch({
+                        type: "CHANGE_CART_QTY",
+                        payload: {
+                          id: prod.id,
+                          qty: e.target.value,
+                        },
+                      })
+                    }
+                  >
                     {[...Array(prod.inStock).keys()].map((x) => (
                       <option key={x + 1}>{x + 1}</option>
                     ))}
@@ -44,7 +56,6 @@ const Cart = () => {
                   <Button
                     type="button"
                     variant="light"
-                    // style={{ cursor: "pointer" }}
                     onClick={() =>
                       dispatch({
                         type: "REMOVE_FROM_CART",
