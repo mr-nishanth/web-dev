d7ff51ddf1858a2f8ea311b2acae2a711f0387c5
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 15:31:24 +0530

    Initial Search Box setup

--- Web-dev/Youtube/ReactJs Materials/redux-toolkit/movie-watchlist/src/features/addMovie/index.jsx
+++ Web-dev/Youtube/ReactJs Materials/redux-toolkit/movie-watchlist/src/features/addMovie/index.jsx
@@ -1,7 +1,45 @@
+import { Search } from "@mui/icons-material";
+import { IconButton, InputBase, Paper } from "@mui/material";
+import { useState } from "react";
+
 const AddMovie = () => {
+  const [query, setQuery] = useState("");
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
-    <div>
-      <h2>Add Movie</h2>
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "570px",
+        alignContent: "center",
+        marginTop: "50px",
+      }}
+    >
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: 540,
+        }}
+      >
+        <InputBase
+          sx={{
+            ml: 1,
+            flex: 1,
+          }}
+          placeholder="Search Movie"
+          value={query}
+          onChange={handleSearch}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="Search">
+          <Search />
+        </IconButton>
+      </Paper>
     </div>
   );
 };
