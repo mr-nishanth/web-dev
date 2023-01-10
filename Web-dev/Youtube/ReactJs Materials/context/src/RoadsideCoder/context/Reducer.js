0d4cf7c85289cc0982ade9395191c5af210a8731
mr-nishanth <innocentnishanth@gmail.com>
2023-01-10 10:46:49 +0530

    SORT TO FILTERS

--- Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
+++ Web-dev/Youtube/ReactJs Materials/context/src/RoadsideCoder/context/Reducer.js
@@ -23,14 +23,14 @@ export const productReducer = (state, action) => {
   switch (action.type) {
     case "SORT_BY_PRICE":
       return { ...state, sort: action.payload };
-    case "SORT_BY_STOCK":
+    case "FILTER_BY_STOCK":
       return { ...state, byStock: !state.byStock };
-    case "SORT_BY_DELIVERY":
+    case "FILTER_BY_DELIVERY":
       return { ...state, byFastDelivery: !state.byFastDelivery };
-    case "SORT_BY_RATING":
-      return { ...state, byRating: state.payload };
+    case "FILTER_BY_RATING":
+      return { ...state, byRating: action.payload };
     case "SORT_BY_SEARCH":
-      return { ...state, searchQuery: state.payload };
+      return { ...state, searchQuery: action.payload };
     case "CLEAR_FILTERS":
       return {
         byStock: false,
