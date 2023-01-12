bff9f0a11b9580ff095d7322d6a2de946a8a0e20
mr-nishanth <innocentnishanth@gmail.com>
2023-01-12 20:35:12 +0530

    normal

---
+++ Web-dev/Youtube/DipeshMalvia/nodejs/03-errorHandling-FS/error.js
@@ -0,0 +1,11 @@
+// Error Object
+
+const error = new Error("Something went wrong");
+console.log();
+// console.log(error);
+console.log();
+// console.log(error.stack);
+console.log();
+// console.log(error.message);
+
+// throw new Error("Error from throw error object");
