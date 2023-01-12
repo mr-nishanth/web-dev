6d3697e9af6f1c5e0890b19922bf09cdfd5161be
mr-nishanth <innocentnishanth@gmail.com>
2023-01-12 16:52:53 +0530

    process exit

---
+++ Web-dev/Youtube/DipeshMalvia/nodejs/01-commandLine/index.js
@@ -0,0 +1,10 @@
+console.log(`\n Nodemon started ⭐`);
+
+console.log("\n");
+
+// code 0 means successfully exit
+// process.exit(0);
+// code1 means forcefully exit its crash process
+//process.exit(1); // [SAME] process.exitCode = 1
+
+// READ ENV variables
