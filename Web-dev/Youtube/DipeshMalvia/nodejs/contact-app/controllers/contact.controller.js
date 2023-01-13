1db3e97ab7a3b15080d6bab293d0d1cb2ff63d71
mr-nishanth <innocentnishanth@gmail.com>
2023-01-13 14:17:36 +0530

    catch handle package added

--- Web-dev/Youtube/DipeshMalvia/nodejs/contact-app/controllers/contact.controller.js
+++ Web-dev/Youtube/DipeshMalvia/nodejs/contact-app/controllers/contact.controller.js
@@ -1,43 +1,49 @@
+const asyncHandler = require("express-async-handler");
+
 /**
  * @description Get all contact
  * @route GET /api/contacts
  * @access public
  */
-exports.getAllContact = (req, res) => {
+exports.getAllContact = asyncHandler(async (req, res) => {
   res.json({ message: "Get All Contact" });
-};
+});
 
 /**
  * @description Get single contact
  * @route GET /api/contacts/:id
  * @access public
  */
-exports.getSingleContact = (req, res) => {};
+exports.getSingleContact = asyncHandler(async (req, res) => {});
 
 /**
  * @description Create a contact
  * @route POST /api/contacts
  * @access public
  */
-exports.createContact = (req, res) => {
+exports.createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are required");
   }
   console.log({ name, email, phone });
-};
+});
 
 /**
  * @description Update a contact
  * @route PUT /api/contacts:id
  * @access public
  */
-exports.updateContact = (req, res) => {};
+exports.updateContact = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+});
 
 /**
  * @description Delete a contact
  * @route DELETE /api/contacts:id
  * @access public
  */
-exports.deleteContact = (req, res) => {};
+exports.deleteContact = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+});
