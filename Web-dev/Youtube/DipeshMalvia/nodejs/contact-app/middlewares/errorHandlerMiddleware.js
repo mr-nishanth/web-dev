ac7aa730698f59fb6ca2247e636eb3e0e6132206
mr-nishanth <innocentnishanth@gmail.com>
2023-01-13 14:16:45 +0530

    custom error handler

---
+++ Web-dev/Youtube/DipeshMalvia/nodejs/contact-app/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,51 @@
+const { constants } = require("../constant");
+const errorHandlerMiddleware = (err, req, res, next) => {
+  const statusCode = res.statusCode ?? 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      return res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+    case constants.UNAUTHORIZED:
+      return res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+
+    case constants.FORBIDDEN:
+      return res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+
+    case constants.NOT_FOUND:
+      return res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+
+    case constants.CONFLICT:
+      return res.json({
+        title: "Conflict",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+    case constants.SERVER_ERROR:
+      return res.json({
+        title: "Internal Server Error",
+        message: err.message,
+        stackTrace: process.env.NODE_ENV !== "production" ? err.stack : null,
+      });
+
+    default:
+      console.log(`No error all good`);
+      break;
+  }
+};
+
+module.exports = errorHandlerMiddleware;
