8add2ebc507c6ef2a95edf9b1831ed3f93dd8d77
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 13:45:00 +0530

    get all admins

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/admin.routes.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/admin.routes.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { login, signUp } from "../controllers/admin.controllers.js";
+import {
+  getAllAdmins,
+  login,
+  signUp,
+} from "../controllers/admin.controllers.js";
 const router = express.Router();
+router.route("/").get(getAllAdmins);
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
 export default router;
