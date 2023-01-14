810acaa7c568c53e2fd1702aeb92f2fab51e7c6e
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 11:11:27 +0530

    added route

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/admin.routes.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/admin.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { login, signUp } from "../controllers/admin.controllers.js";
 const router = express.Router();
-
+router.route("/signup").post(signUp);
+router.route("/login").post(login);
 export default router;
