8c77f4f65d3708c7d3eb7eb7b02acb3693e5ba64
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 11:11:38 +0530

    jwt creation

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/utils/helper.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/utils/helper.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const encryptedPassword = async (password, saltCount = 10) => {
   const salt = saltCount;
   const hashPassword = await bcrypt.hash(password, salt);
@@ -8,4 +9,11 @@ export const encryptedPassword = async (password, saltCount = 10) => {
 export const decryptedPassword = async (password, userPassword) => {
   const compare = await bcrypt.compare(password, userPassword);
   return compare;
+};
+
+export const createToken = async (id) => {
+  const token = await jwt.sign({ id }, process.env.JWT_ACCESS_TOKEN, {
+    expiresIn: "7d",
+  });
+  return token;
 };
