16dd83dd6244f39fa5d72ff0e678e244bf505396
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 07:15:28 +0530

    sign up and get all user

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/controllers/user.controllers.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/controllers/user.controllers.js
@@ -1,4 +1,5 @@
-import User from "../models/User.model";
+import User from "../models/User.model.js";
+import { encryptedPassword } from "../utils/helper.js";
 
 /**
  * @description Get all users
@@ -18,4 +19,41 @@ export const getAllUsers = async (req, res, next) => {
   }
 
   return res.status(200).json({ users });
+};
+
+/**
+ * @description Create a new user
+ * @route POST /api/users
+ * @access public
+ */
+export const signUp = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  //^ Validate the user details
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid User details" });
+  }
+  const hashPassword = await encryptedPassword(password);
+  console.log(`Hash password: ${hashPassword}`);
+  let newUser;
+  try {
+    newUser = new User({ name, email, password: hashPassword });
+    newUser = await newUser.save();
+  } catch (error) {
+    return console.log(error);
+    // return next(error);
+  }
+
+  if (!newUser) {
+    return res.status(500).json({ message: "Unexpected Error Occurred" });
+  }
+
+  return res.status(201).json({ id: newUser.id, email: newUser.email });
 };
