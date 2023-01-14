9d797c21e4569cce5169b169f26b86cc3f21d7c2
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 17:13:04 +0530

    get all booking

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/user.routes.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/routes/user.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   deleteUser,
   getAllUsers,
+  getBookingsOfUser,
   login,
   signUp,
   updateUser,
@@ -12,5 +13,5 @@ router.route("/").get(getAllUsers);
 router.route("/signup").post(signUp);
 router.route("/login").post(login);
 router.route("/:id").put(updateUser).delete(deleteUser);
-
+router.get("/bookings/:id", getBookingsOfUser);
 export default router;
