88ba0691ede5ea54226126b9561df08dee420eee
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 22:23:42 +0530

    convert to throw

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/controllers/user.controllers.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js";
 
-export const registerUser = async (req, res) => {
+import asyncHandler from "express-async-handler";
+export const registerUser = asyncHandler(async (req, res) => {
   const { email } = req.body;
   //^ Check if the user is already registered
   const isExisting = await User.findOne({ email }).lean();
@@ -11,8 +12,10 @@ export const registerUser = async (req, res) => {
     return res.status(201).json({ newUser, success: true });
   } else {
     // The existing user
-    return res
-      .status(400)
-      .json({ msg: "User already registered", success: false });
+    // return res
+    //   .status(400)
+    //   .json({ msg: "User already registered", success: false });
+    res.status(400);
+    throw new Error("User already registered");
   }
-};
+});
