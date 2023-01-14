07f42281bb1beadedd0a70f85f13038acf6a7f13
mr-nishanth <innocentnishanth@gmail.com>
2023-01-14 10:38:56 +0530

    initial admin routes

--- Web-dev/Youtube/MERN_Stack/IndianCoders/backend/models/Admin.model.js
+++ Web-dev/Youtube/MERN_Stack/IndianCoders/backend/models/Admin.model.js
@@ -1,6 +1,22 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema({});
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: [true, "Email must be unique"],
+    required: [true, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a  password"],
+    minLength: [6, "Password must be at least 6 characters"],
+  },
+  addedMovies: [
+    {
+      type: String,
+    },
+  ],
+});
 
 const adminModel = mongoose.model("Admin", adminSchema);
 
