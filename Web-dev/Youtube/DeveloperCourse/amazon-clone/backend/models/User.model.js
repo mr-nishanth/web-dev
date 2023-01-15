e1b829d148659582ef7e012ff6df242b3fe60fac
mr-nishanth <innocentnishanth@gmail.com>
2023-01-15 09:50:05 +0530

    role added

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/models/User.model.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/models/User.model.js
@@ -28,13 +28,25 @@ const userSchema = new mongoose.Schema({
     trim: true,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["admin", "user", "guest"],
+    default: "user",
+  },
 });
 
+// Password hashing
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// Compare the hashed password
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  const status = await bcrypt.compare(enteredPassword, this.password);
+  return status;
+};
 
 const userModel = mongoose.model("User", userSchema);
 
