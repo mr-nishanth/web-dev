25bcb96e5e10d29574618c2a6066c82642897275
mr-nishanth <innocentnishanth@gmail.com>
2023-01-15 14:53:13 +0530

    refresh and logout

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
+const router = express.Router();
 
 // Controller Imports
 import {
   deleteUser,
   getAllUser,
   getOneUser,
+  handleRefreshToken,
   loginUser,
+  logout,
   registerUser,
   updatedUser,
 } from "../controllers/user.controllers.js";
@@ -16,21 +19,19 @@ import {
   isAdmin,
   unBlockUser,
 } from "../middleware/authMiddleware.js";
-const router = express.Router();
+
+// Authentication and authorization routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 //~ Protected routes via auth middleware
 
-// router
-//   .route("/:id")
-//   .get([isAdmin, getOneUser])
-//   .put(updatedUser)
-//   .delete(deleteUser);
 router.get("/all-user", [authMiddleware, isAdmin, getAllUser]);
 router.get("/one-user", [authMiddleware, getOneUser]);
 router.put("/edit-user", [authMiddleware, isAdmin, updatedUser]);
 router.delete("/delete-user", [authMiddleware, deleteUser]);
 router.put("/block-user/:id", [authMiddleware, isAdmin, blockUser]);
 router.put("/unblock-user/:id", [authMiddleware, isAdmin, unBlockUser]);
+router.get("/refresh", [handleRefreshToken]);
+router.get("/logout", [logout]);
 export default router;
