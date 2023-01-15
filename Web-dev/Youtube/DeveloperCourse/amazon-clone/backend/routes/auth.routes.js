16e5206c02c3803cfd7b482ad479902e385f09a3
mr-nishanth <innocentnishanth@gmail.com>
2023-01-15 12:39:54 +0530

    middleware added

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+
+// Controller Imports
 import {
   deleteUser,
   getAllUser,
@@ -7,9 +9,28 @@ import {
   registerUser,
   updatedUser,
 } from "../controllers/user.controllers.js";
+// Middleware Imports
+import {
+  authMiddleware,
+  blockUser,
+  isAdmin,
+  unBlockUser,
+} from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/all-user", getAllUser);
-router.route("/:id").get(getOneUser).put(updatedUser).delete(deleteUser);
+
+//~ Protected routes via auth middleware
+
+// router
+//   .route("/:id")
+//   .get([isAdmin, getOneUser])
+//   .put(updatedUser)
+//   .delete(deleteUser);
+router.get("/all-user", [authMiddleware, isAdmin, getAllUser]);
+router.get("/one-user", [authMiddleware, getOneUser]);
+router.put("/edit-user", [authMiddleware, isAdmin, updatedUser]);
+router.delete("/delete-user", [authMiddleware, deleteUser]);
+router.put("/block-user/:id", [authMiddleware, isAdmin, blockUser]);
+router.put("/unblock-user/:id", [authMiddleware, isAdmin, unBlockUser]);
 export default router;
