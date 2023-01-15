8983df42249e603bcf2ca09f27197ffb07393876
mr-nishanth <innocentnishanth@gmail.com>
2023-01-15 10:30:48 +0530

    get all , one , delete and update

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
@@ -1,6 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import {
+  deleteUser,
+  getAllUser,
+  getOneUser,
+  loginUser,
+  registerUser,
+  updatedUser,
+} from "../controllers/user.controllers.js";
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/all-user", getAllUser);
+router.route("/:id").get(getOneUser).put(updatedUser).delete(deleteUser);
 export default router;
