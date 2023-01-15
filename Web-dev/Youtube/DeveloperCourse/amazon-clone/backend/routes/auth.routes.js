c0d8d2d270f3d13544ed71b312cd75cb3926fa1f
mr-nishanth <innocentnishanth@gmail.com>
2023-01-15 09:50:46 +0530

    login

--- Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
+++ Web-dev/Youtube/DeveloperCourse/amazon-clone/backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 const router = express.Router();
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 export default router;
