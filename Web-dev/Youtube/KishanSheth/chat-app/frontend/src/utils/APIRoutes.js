0bbfe83b2714b5e51114b723f910b59e0c9dd04e
mr-nishanth <innocentnishanth@gmail.com>
2023-01-16 10:43:00 +0530

    routes

---
+++ Web-dev/Youtube/KishanSheth/chat-app/frontend/src/utils/APIRoutes.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+const host = "http://localhost:3500";
+axios.defaults.baseURL = host;
+export const registerRoute = `/api/auth/register`;
+export const loginRoute = `/api/auth/login`;
+export const chatRoute = `/api/auth/login`;
