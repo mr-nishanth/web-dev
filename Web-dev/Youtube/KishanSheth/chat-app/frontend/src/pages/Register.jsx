3d492ef0abfec336bacd9b2a1766119cad443073
mr-nishanth <innocentnishanth@gmail.com>
2023-01-16 12:30:51 +0530

    useEffect added

--- Web-dev/Youtube/KishanSheth/chat-app/frontend/src/pages/Register.jsx
+++ Web-dev/Youtube/KishanSheth/chat-app/frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
