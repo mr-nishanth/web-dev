726048122b96bae4d66a6f4f32e461aeabc5a58b
mr-nishanth <innocentnishanth@gmail.com>
2023-01-16 10:42:52 +0530

    route setup

--- Web-dev/Youtube/KishanSheth/chat-app/frontend/src/pages/Register.jsx
+++ Web-dev/Youtube/KishanSheth/chat-app/frontend/src/pages/Register.jsx
@@ -1,12 +1,60 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 const Register = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("FORM");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  let toastOptions = {
+    position: "bottom-right",
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
   };
-  const handleChange = (e) => {};
+  const handleValidation = () => {
+    const { username, email, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      toast.error(`Password and confirm password should be same`, toastOptions);
+      return false;
+    } else if (username.length < 3) {
+      toast.error(`Username should be greater then 3 characters`, toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        `Password should be equal or greater 8 characters`,
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error(`Email is required`, toastOptions);
+      return true;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      const { username, email, password, confirmPassword } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+    }
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <FormContainer>
@@ -45,6 +93,7 @@ const Register = () => {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 };
