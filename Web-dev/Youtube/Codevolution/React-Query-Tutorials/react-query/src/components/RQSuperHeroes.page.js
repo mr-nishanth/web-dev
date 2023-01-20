47e7fd4a9a96c8e93057ecf9ff03229ae53300d6
mr-nishanth <innocentnishanth@gmail.com>
2023-01-20 11:40:14 +0530

    react query dev tools setup

--- Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/components/RQSuperHeroes.page.js
+++ Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/components/RQSuperHeroes.page.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const fetchSuperHeroes = () => {
-  // return axios.get("http://localhost:4000/superheroes");
-  return axios.get("http://localhost:4000/superheroes1");
+  return axios.get("http://localhost:4000/superheroes");
+  // return axios.get("http://localhost:4000/superheroes1");
 };
 
 export const RQSuperHeroesPage = () => {
--- Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/index.js
+++ Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 const query = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={query}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
