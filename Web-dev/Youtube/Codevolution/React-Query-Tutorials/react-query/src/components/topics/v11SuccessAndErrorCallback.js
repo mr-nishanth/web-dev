0ae3587e51f93d8872969fd500a26db124dacfbb
mr-nishanth <innocentnishanth@gmail.com>
2023-01-20 15:10:11 +0530

    Success and Error Callbacks

--- Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/components/topics/v11SuccessAndErrorCallback.js
+++ Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/components/topics/v11SuccessAndErrorCallback.js
@@ -45,3 +45,16 @@ export const RQSuperHeroesPage = () => {
     </>
   );
 };
+
+/*
+  Home Work
+
+  Polling + Callbacks
+
+  Use refetchInterval options to poll the api data every 3 sec , behind the scenes add fourth superhero of your choice to the superheros array in db.json
+  within the onSuccess callback check if the number  of heroes is 4 and if it is the case i want you to stop the polling ,
+   if there is an error as well i want to stop the polling
+
+   HINT:
+   Maintain the state variable initial value 3000 and the state variable will be assigned to refetchInterval configuration inside the callback functions check for the response or error and set the value to false that will stop your polling
+*/
