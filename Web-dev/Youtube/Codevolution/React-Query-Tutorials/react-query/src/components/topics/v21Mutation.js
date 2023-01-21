7ba9e1f9d9c518ba9a85ab5413595df868f94587
mr-nishanth <innocentnishanth@gmail.com>
2023-01-21 10:09:15 +0530

    useMutation

---
+++ Web-dev/Youtube/Codevolution/React-Query-Tutorials/react-query/src/components/topics/v21Mutation.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import {
+  useAddSuperHeroData,
+  useSuperHeroesData,
+} from "../hooks/useSuperHeroesData";
+
+export const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+  const onSuccess = (data) => {
+    console.log(
+      `Performing sideEffects eg:Toast notifications ${JSON.stringify(data)}`
+    );
+  };
+  const onError = (error) => {
+    console.log(
+      `Performing sideEffects after encountered error: ${error.message}`
+    );
+  };
+  const {
+    isLoading,
+    isFetching,
+    refetch,
+    isError,
+    error,
+    data: superHeroes,
+  } = useSuperHeroesData(onSuccess, onError);
+  const {
+    mutate: addHero,
+    isLoading: isAddHeroLoading,
+    isError: isAddHeroError,
+    error: addHeroError,
+  } = useAddSuperHeroData();
+  // console.log({ superHeroes });
+
+  const handleAddHeroClick = () => {
+    console.log({ name, alterEgo });
+    const hero = { name, alterEgo };
+    addHero(hero);
+  };
+  console.log({ isFetching, isLoading });
+  if (isLoading || isFetching) return <h1>Loading....</h1>;
+  if (isError) return <h1>{error.message}</h1>;
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      <button onClick={refetch}>Fetch Super Heroes</button>
+      {superHeroes?.data?.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+/**
+ * Create brand new components and display the list of superheroes using custom query hook , we have just built,
+ * however i also want you to ensure the data is fetched when the component mounts in react query superheros page
+ * where as the data should only be fetched , when you click a button in the new component
+ */
