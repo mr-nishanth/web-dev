0a5c1eb704cf322a89ae11c13bef4ee7bdb811c3
mr-nishanth <innocentnishanth@gmail.com>
2023-01-24 15:20:49 +0530

    modal completed

--- Web-dev/Youtube/CodingInFlow/mern-stack/frontend/src/App.tsx
+++ Web-dev/Youtube/CodingInFlow/mern-stack/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Note from "./components/Note";
 import { Note as NoteModel } from "./models/notes.model";
 import styles from "./styles/NotesPage.module.css";
 import * as NotesApi from "./services/notes_api";
+import AddNoteDialog from "./components/AddNoteDialog";
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     async function loadNotes() {
@@ -22,6 +24,7 @@ function App() {
 
   return (
     <Container>
+      <Button onClick={() => setShowAddNoteDialog(true)}>Add new note</Button>
       <Row xs={1} md={2} xl={3} className="g-4">
         {notes.map((note) => (
           <Col key={note._id}>
@@ -29,6 +32,10 @@ function App() {
           </Col>
         ))}
       </Row>
+
+      {showAddNoteDialog && (
+        <AddNoteDialog onDismiss={() => setShowAddNoteDialog(false)} />
+      )}
     </Container>
   );
 }
