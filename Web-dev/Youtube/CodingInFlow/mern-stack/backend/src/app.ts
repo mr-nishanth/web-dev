052149914cd02d05d08849ec3a0b0de393fcc45f
mr-nishanth <innocentnishanth@gmail.com>
2023-01-24 10:40:49 +0530

    error handling

--- Web-dev/Youtube/CodingInFlow/mern-stack/backend/src/app.ts
+++ Web-dev/Youtube/CodingInFlow/mern-stack/backend/src/app.ts
@@ -5,6 +5,7 @@ import notesRoutes from "./routes/notes.routes";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import createHttpError, { isHttpError } from "http-errors";
 const app = express();
 
 // MIDDLEWARE
@@ -18,17 +19,20 @@ app.use("/api/notes", notesRoutes);
 
 // ERROR HANDLING
 app.use((req, res, next) => {
-  next(new Error("Path Not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occurred";
-  if (error instanceof Error) {
+  let statusCode = 500;
+
+  if (isHttpError(error)) {
+    statusCode = error.status;
     errorMessage = error.message;
   }
-  res.status(500).json({ error: errorMessage });
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
--- Web-dev/Youtube/CodingInFlow/mern-stack/backend/src/controllers/notes.controller.ts
+++ Web-dev/Youtube/CodingInFlow/mern-stack/backend/src/controllers/notes.controller.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 import Note from "../models/Node.model";
 export const getNotes: RequestHandler = async (req, res, next) => {
   try {
@@ -12,16 +14,37 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 export const getNote: RequestHandler = async (req, res, next) => {
   const { noteId } = req.params;
   try {
-    const notes = await Note.findById(noteId).exec();
-    res.status(200).json(notes);
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid noteId");
+    }
+
+    const note = await Note.findById(noteId).exec();
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
+    res.status(200).json(note);
   } catch (error) {
     next(error);
   }
 };
 
-export const createNote: RequestHandler = async (req, res, next) => {
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
   const { title, text } = req.body;
   try {
+    if (!title || title.trim() === "") {
+      throw createHttpError(400, "Title is required");
+    }
+
     const newNote = await Note.create({ title, text });
     res.status(201).json(newNote);
   } catch (error) {
