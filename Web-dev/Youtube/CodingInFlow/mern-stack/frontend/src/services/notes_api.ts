de849abfcf276903535d357262c97939984e3480
mr-nishanth <innocentnishanth@gmail.com>
2023-01-24 15:09:23 +0530

    create notes

--- Web-dev/Youtube/CodingInFlow/mern-stack/frontend/src/services/notes_api.ts
+++ Web-dev/Youtube/CodingInFlow/mern-stack/frontend/src/services/notes_api.ts
@@ -17,3 +17,19 @@ export async function fetchNotes(): Promise<Note[]> {
   });
   return response.json();
 }
+
+export interface NoteInput {
+  title: string;
+  text: string;
+}
+
+export async function createNote(note: NoteInput): Promise<Note> {
+  const response = await fetchData("http://localhost:5000/api/notes/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  return response.json();
+}
