b66e02b6dcecfe456078c76d6d7e2ec622d432c7
mr-nishanth <innocentnishanth@gmail.com>
2023-02-08 17:10:37 +0530

    custom errors

--- Web-dev/Youtube/ReactJs Materials/react-hooks-form/src/components/Register.jsx
+++ Web-dev/Youtube/ReactJs Materials/react-hooks-form/src/components/Register.jsx
@@ -3,7 +3,12 @@ import { useForm } from "react-hook-form";
 import "../App.css";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  console.log({ errors });
   return (
     <div className="App">
       <div className="title">Sign Up</div>
@@ -13,8 +18,24 @@ const Register = () => {
           console.log({ data });
         })}
       >
-        <input type="text" placeholder="First Name" {...register("fName")} />
-        <input type="text" placeholder="Last Name" {...register("lName")} />
+        <input
+          type="text"
+          placeholder="First Name"
+          {...register("fName", { required: "Firstname is required" })}
+        />
+        {errors?.fName && <p>{errors?.fName?.message}</p>}
+        <input
+          type="text"
+          placeholder="Last Name"
+          {...register("lName", {
+            required: "Lastname is required",
+            minLength: {
+              value: 4,
+              message: "Lastname must be at least 4 characters",
+            },
+          })}
+        />
+        {errors?.lName && <p>{errors?.lName?.message}</p>}
         <input type="submit" value="Submit" id="submit" />
       </form>
     </div>
