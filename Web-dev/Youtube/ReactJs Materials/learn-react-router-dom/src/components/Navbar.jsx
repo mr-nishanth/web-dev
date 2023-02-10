ccfacf3e460e7aea7f90b2efa8d2e67d0416037a
mr-nishanth <innocentnishanth@gmail.com>
2023-02-10 15:54:43 +0530

    added style for NavLink

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/src/components/Navbar.jsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/src/components/Navbar.jsx
@@ -3,18 +3,26 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import { Container } from "@mui/material";
+import { Container, styled } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
+const StyledNavLink = styled(NavLink)({
+  textDecoration: "none",
+  color: "white",
+  fontSize: 20,
+  "&.active": {
+    background: "navy",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Container>
           <Toolbar>
-            <Typography variant="h4" sx={{ flexGrow: 1 }}>
+            <Typography variant="h3" sx={{ flexGrow: 1 }}>
               Blackly
             </Typography>
             <Box
@@ -25,10 +33,10 @@ export default function ButtonAppBar() {
                 gap: "5rem",
               }}
             >
-              <NavLink to={"/"}>Home</NavLink>
-              <NavLink to={"about"}>About</NavLink>
-              <NavLink to={"contact"}>Contact</NavLink>
-              <NavLink to={"career"}>Career</NavLink>
+              <StyledNavLink to={"/"}>Home</StyledNavLink>
+              <StyledNavLink to={"about"}>About</StyledNavLink>
+              <StyledNavLink to={"contact"}>Contact</StyledNavLink>
+              <StyledNavLink to={"career"}>Career</StyledNavLink>
             </Box>
           </Toolbar>
         </Container>
