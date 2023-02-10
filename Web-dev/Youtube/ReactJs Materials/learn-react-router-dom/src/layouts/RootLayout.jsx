98b04b865820c305c8e4d000da55a66d8e4c5521
mr-nishanth <innocentnishanth@gmail.com>
2023-02-10 18:05:20 +0530

    added AppBreadcrumbs

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/src/layouts/RootLayout.jsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/src/layouts/RootLayout.jsx
@@ -1,11 +1,15 @@
 import { Container } from "@mui/system";
 import { Outlet } from "react-router-dom";
+import AppBreadcrumbs from "../components/AppBreadcrumbs";
 import Navbar from "../components/Navbar";
 const RootLayout = () => {
   return (
     <>
       <Navbar />
-      <Container sx={{ padding: "1rem" }}>
+      <Container sx={{ p: 3 }}>
+        <AppBreadcrumbs />
+      </Container>
+      <Container sx={{ p: 5 }}>
         <Outlet />
       </Container>
     </>
