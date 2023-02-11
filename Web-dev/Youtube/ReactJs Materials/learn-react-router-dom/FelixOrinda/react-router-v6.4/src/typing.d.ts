0e86591523c0cba6ef37e34c74d958f2ca7f51c9
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 10:24:10 +0530

    added UserType and PostType

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/typing.d.ts
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/typing.d.ts
@@ -1,5 +1,30 @@
 export type UserType = {
-  id?: string | number;
+  id: number;
   name: string;
+  username: string;
   email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export type PostType = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 };
