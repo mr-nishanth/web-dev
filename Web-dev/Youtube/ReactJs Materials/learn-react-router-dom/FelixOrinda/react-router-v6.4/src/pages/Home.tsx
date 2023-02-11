de9aa670dbb51e3aa8cadd6c56f222cc6b6965ed
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 12:57:01 +0530

    retrieve state from useLocation

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Home.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const state = useLocation().state;
+  console.log({ state });
+  const navigate = useNavigate();
+  // navigate("/dashboard", { state: { from: "/home" } });
+  // navigate("/about");
+
   return <div>Home</div>;
 };
 export default Home;
