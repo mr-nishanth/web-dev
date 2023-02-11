6dcffedacdf9b8495e482ff2cd497f29ce35ab52
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 09:55:25 +0530

    added NavLink

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/components/Layout.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/components/Layout.tsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 type Props = {
   children: JSX.Element;
 };
+const links = [
+  { id: "Home", path: "/", label: "Home" },
+  { id: "About", path: "/about", label: "About" },
+  { id: "Contact", path: "/contact", label: "Contact" },
+  {
+    id: "Dashboard",
+    path: "/dashboard",
+    label: "Dashboard",
+  },
+];
 const Layout = ({ children }: Props) => {
   return (
     <div>
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-        <Link to={"/dashboard"}>Dashboard</Link>
+      <nav className="flex justify-center gap-4 border-b py-4 shadow">
+        {links.map((link) => (
+          <NavLink
+            to={link.path}
+            key={link.id}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "text-gray-600"
+            }
+          >
+            {link.label}
+          </NavLink>
+        ))}
       </nav>
       {children}
     </div>
