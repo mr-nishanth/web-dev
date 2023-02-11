723f32047801cd5c7963e0b3779594dda08ff184
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 09:05:48 +0530

    added basic routes

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/App.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/App.tsx
@@ -1,6 +1,7 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-export default App
+  return <RouterProvider router={router} />;
+};
+export default App;
---
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/router/index.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Dashboard from "../pages/Dashboard";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/about",
+    element: (
+      <Layout>
+        <About />
+      </Layout>
+    ),
+  },
+  {
+    path: "/contact",
+    element: (
+      <Layout>
+        <Contact />
+      </Layout>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <Layout>
+        <Dashboard />
+      </Layout>
+    ),
+  },
+]);
