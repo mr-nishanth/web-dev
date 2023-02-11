3fee2995c9cdb580d583305fdf4ae795ec417d6e
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 12:55:41 +0530

    added loader data

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/DashboardStats.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/DashboardStats.tsx
@@ -1,4 +1,15 @@
+import { useLoaderData, useNavigation } from "react-router-dom";
+import { PostType } from "../typing";
+
 const DashboardStats = () => {
-  return <div>DashboardStats</div>;
+  const { posts } = useLoaderData() as { posts: PostType[] };
+  const navigation = useNavigation() as any;
+  if (navigation.state === "loading") return <p>Loading...</p>;
+  return (
+    <div>
+      <h1>DashboardStats</h1>
+      <p className="text-gray-700">There are {posts?.length} posts</p>
+    </div>
+  );
 };
 export default DashboardStats;
