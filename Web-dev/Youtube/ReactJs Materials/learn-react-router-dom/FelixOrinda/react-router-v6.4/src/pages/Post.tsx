b532b8dd78cff4c9c20a86b453a59d2350c860e7
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 12:57:25 +0530

    added loader data

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Post.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Post.tsx
@@ -1,4 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { PostType } from "../typing";
+
 const Post = () => {
-  return <div>Post</div>;
+  const { post } = useLoaderData() as { post: PostType | undefined };
+  // console.log({ post });
+  return (
+    <div className="p-10">
+      {post ? (
+        <div>
+          <h1 className="font-bold uppercase ">{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 export default Post;
--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Posts.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/pages/Posts.tsx
@@ -1,4 +1,23 @@
+import { Link, useLoaderData, useNavigation } from "react-router-dom";
+import { PostType } from "../typing";
+
 const Posts = () => {
-  return <div>Posts</div>;
+  const { posts } = useLoaderData() as { posts: PostType[] };
+  const navigation = useNavigation() as any;
+  // console.log(navigation.state);
+  // console.log(navigation.location);
+  if (navigation.state === "loading") return <p>Loading...</p>;
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">Posts</h1>
+      <ol className="list-disc px-8 capitalize">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/dashboard/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 export default Posts;
