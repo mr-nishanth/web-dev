b23f956e2c0ae5b4578233de783fe287bff1fa84
mr-nishanth <innocentnishanth@gmail.com>
2023-02-11 09:05:11 +0530

    added tsx and ts to apply the tailwind styled

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/tailwind.config.cjs
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.tsx", "./src/**/*.ts"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
