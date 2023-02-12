2c5b92ae016d8ad61869d813979d1b62e9e8f186
mr-nishanth <innocentnishanth@gmail.com>
2023-02-12 20:46:35 +0530

    added Modal

--- Web-dev/Youtube/Tamil-Coding-Wizard/bootstrap-react-tut/src/components/BlackCard.jsx
+++ Web-dev/Youtube/Tamil-Coding-Wizard/bootstrap-react-tut/src/components/BlackCard.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Image from "../assets/card-img.jpg";
+import ModalScreen from "./ModalScreen";
 const BlackCard = () => {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <Card className="m-3">
-      <Card.Img variant="top" src={Image} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <div className="text-center">
-          <Button variant="primary">Add to Cart</Button>
-        </div>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className="m-3">
+        <Card.Img variant="top" src={Image} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <div className="text-center">
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+              Add to Cart
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+      {modalShow && (
+        <ModalScreen show={modalShow} onHide={() => setModalShow(false)} />
+      )}
+    </>
   );
 };
 export default BlackCard;
