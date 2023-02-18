d293a51b2aba60d9eefffd91107f022b31ef9e2b
mr-nishanth <innocentnishanth@gmail.com>
2023-02-18 20:31:54 +0530

    git date change

--- git-replace.sh
+++ git-replace.sh
@@ -16,3 +16,8 @@ then
 	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
+
+
+# git add -A
+# git commit -m "commit message"
+# GIT_COMMITTER_DATE="Wed Sep 9 22:00 2020 +0530" git commit --amend --date="Wed Sep 9 22:00 2020 +0530"
