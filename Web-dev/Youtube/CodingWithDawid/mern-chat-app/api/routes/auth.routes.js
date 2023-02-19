2fb85ee97edd1ec623a18d594ab848df13a19180
mr-nishanth <innocentnishanth@gmail.com>
2023-02-19 22:14:51 +0530

    added routes for profile

--- Web-dev/Youtube/CodingWithDawid/mern-chat-app/api/routes/auth.routes.js
+++ Web-dev/Youtube/CodingWithDawid/mern-chat-app/api/routes/auth.routes.js
@@ -2,4 +2,5 @@ import express from "express";
 const router = express.Router();
 import * as auth from "../controllers/auth.controllers.js";
 router.post("/register", auth.register);
+router.get("/profile", auth.profile);
 export default router;
