3fc6a7b990fd878a652c0156e56313f5d522be54
mr-nishanth <innocentnishanth@gmail.com>
2023-02-19 18:20:34 +0530

    added register

--- Web-dev/Youtube/CodingWithDawid/mern-chat-app/api/controllers/auth.controllers.js
+++ Web-dev/Youtube/CodingWithDawid/mern-chat-app/api/controllers/auth.controllers.js
@@ -3,7 +3,7 @@ import User from "../models/User.model.js";
 
 export const register = async (req, res) => {
   const { username, password } = req.body;
-  //   CHECK 422 ERROR
+  //   CHECK 422 unprocessable entity
   if (!username || !password) {
     return res.status(422).json({ error: "Please add all the fields" });
   }
@@ -19,12 +19,19 @@ export const register = async (req, res) => {
       { expiresIn: "12h" },
       (err, token) => {
         if (err) throw err;
-        res.cookie("token", token, { httpOnly: true }).json("OK");
+        res.cookie("token", token, { httpOnly: true }).json({
+          id: user._id,
+        });
       }
     );
     console.log({ user });
   } catch (error) {
     console.log(error);
+    // 11000 is the error code for duplicate key
+    if (error.code === 11000) {
+      return res.status(422).json({ error: "Username already exists" });
+    }
+
     res.status(500).json({ error: "Server error" });
   }
 };
