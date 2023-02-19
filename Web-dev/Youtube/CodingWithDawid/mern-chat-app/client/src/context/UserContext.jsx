d73b2287469198dc49051c8dfada7f714d59240f
mr-nishanth <innocentnishanth@gmail.com>
2023-02-19 22:17:18 +0530

    added user check

--- Web-dev/Youtube/CodingWithDawid/mern-chat-app/client/src/context/UserContext.jsx
+++ Web-dev/Youtube/CodingWithDawid/mern-chat-app/client/src/context/UserContext.jsx
@@ -1,10 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { request } from "../utils/axios-utils";
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState({
     username: "",
     id: "",
   });
+
+  useEffect(() => {
+    request({
+      url: "/profile",
+      method: "GET",
+    })
+      .then((res) => {
+        const { username, id } = res?.data;
+        console.log({ username, id });
+        setUserData({ username, id });
+      })
+      .catch((err) => {});
+  }, []);
+
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
       {children}
