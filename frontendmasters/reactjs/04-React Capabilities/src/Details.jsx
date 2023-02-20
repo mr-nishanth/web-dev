445ed11abd22631e572f26a5e3b204883e9aee92
mr-nishanth <innocentnishanth@gmail.com>
2023-02-20 17:00:14 +0530

    added modal

--- frontendmasters/reactjs/04-React Capabilities/src/Details.jsx
+++ frontendmasters/reactjs/04-React Capabilities/src/Details.jsx
@@ -2,7 +2,10 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import ErrorBoundary from "./ErrorBoundary";
+import { useState } from "react";
+import Modal from "./Modal";
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
   if (results.isError) {
@@ -27,9 +30,20 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>;
         <p>{pet.description}</p>
       </div>
+      {showModal ? (
+        <Modal>
+          <div>
+            <h1>Would you like to adopt {pet.name}?</h1>
+            <div className="buttons">
+              <button>Yes</button>
+              <button onClick={() => setShowModal(false)}>No</button>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   );
 };
