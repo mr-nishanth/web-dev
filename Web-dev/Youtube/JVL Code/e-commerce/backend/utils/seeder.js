2413d5b494c705468b89f053e9837293681c6853
mr-nishanth <innocentnishanth@gmail.com>
2023-02-21 22:02:34 +0530

    added comment

--- Web-dev/Youtube/JVL Code/e-commerce/backend/utils/seeder.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/utils/seeder.js
@@ -1,26 +1,25 @@
-const dotenv = require("dotenv")
-const path = require("path")
-dotenv.config({ path: path.join(__dirname, "..", "config", "config.env") })
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({ path: path.join(__dirname, "..", "config", "config.env") });
 
-const connectDB = require("../helpers/databases")
+const connectDB = require("../helpers/databases");
 
-const products = require("../data/products.json")
-const Product = require("../model/product.model")
+//* Above imports is mandatory to run Database connection [get the environment variables]
+connectDB();
 
-// Database connection
-connectDB()
-
+const products = require("../data/products.json");
+const Product = require("../model/product.model");
 
 const seedProducts = async () => {
-    try {
-        const deletedProducts = await Product.deleteMany({})
-        console.log(`➖➖ All Products ➖➖ `)
-        const insertedProducts = await Product.insertMany(products)
-        console.log(`➕➕ All Products ➕➕ `)
-    } catch (error) {
-        console.log(error.message)
-    }
-    process.exit()
-}
+  try {
+    const deletedProducts = await Product.deleteMany({});
+    console.log(`➖➖ All Products ➖➖ `);
+    const insertedProducts = await Product.insertMany(products);
+    console.log(`➕➕ All Products ➕➕ `);
+  } catch (error) {
+    console.log(error.message);
+  }
+  process.exit();
+};
 
-seedProducts()
+seedProducts();
