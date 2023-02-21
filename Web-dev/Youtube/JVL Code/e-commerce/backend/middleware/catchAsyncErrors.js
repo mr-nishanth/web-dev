da8a67a1cc23a6f49d4640e16c6a22bf437b5ded
mr-nishanth <innocentnishanth@gmail.com>
2023-02-21 22:53:50 +0530

    added comment

--- Web-dev/Youtube/JVL Code/e-commerce/backend/middleware/catchAsyncErrors.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/middleware/catchAsyncErrors.js
@@ -1,7 +1,6 @@
+// fun is a function that returns a promise and we want to catch errors in that function and pass it to the next middleware which is error middleware in this case (backend\middleware\error.js)
+//* This middleware is used in backend\controllers\* to catch errors in async functions
 module.exports = (fun) => (req, res, next) => {
-    return Promise
-        .resolve(
-            fun(req, res, next)
-            // here catch(next) is errors middleware
-        ).catch(next)
-}
+  return Promise.resolve(fun(req, res, next)).catch(next);
+  // here catch(next) pass the request to next middleware (errors middleware)
+};
