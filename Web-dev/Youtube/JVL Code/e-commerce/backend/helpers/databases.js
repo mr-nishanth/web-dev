1516afb15caa9193b78ddfcf4bf82f5faf458e76
mr-nishanth <innocentnishanth@gmail.com>
2023-02-21 21:45:08 +0530

    added catch block

--- Web-dev/Youtube/JVL Code/e-commerce/backend/helpers/databases.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/helpers/databases.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', true);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 const connectDB = async () => {
-    const MONGO_URI = process.env.MONGODB_LOCAL
-    console.log(`🥭 Database URI : ${MONGO_URI}`)
+  const MONGO_URI = process.env.MONGODB_LOCAL;
+  console.log(`\n 🥭 Database URI : ${MONGO_URI}`);
 
-    const result = await mongoose.connect(MONGO_URI, {
-        // useNewUrlParams: true,
-        useUnifiedTopology: true
-    }).then((result) => {
-        // console.log(`MongoDB is connected to the host : ${result.connection.host}`)
-        console.log(`\n MongoDB is connected to the host : ${result.connection.host} \n`)
+  const result = await mongoose
+    .connect(MONGO_URI, {
+      // useNewUrlParams: true,
+      useUnifiedTopology: true,
     })
+    .then((result) => {
+      console.log(
+        `\n MongoDB is connected to the host : ${result.connection.host} \n`
+      );
+    })
+    .catch((err) => {
+      console.log(`\n 🥭 Error : ${err.message} \n`);
+      process.exit(1);
+    });
+};
 
-}
-
-module.exports = connectDB
+module.exports = connectDB;
