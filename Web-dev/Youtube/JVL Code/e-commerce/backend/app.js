ed369be8313f4f112f25ee7126c9e7c6bd22c379
mr-nishanth <innocentnishanth@gmail.com>
2023-02-21 20:43:49 +0530

    added helmet

--- Web-dev/Youtube/JVL Code/e-commerce/backend/app.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/app.js
@@ -1,28 +1,26 @@
-
-const express = require('express');
-const cors = require("cors")
-const logger = require("morgan")
-const errorMiddleware = require("./middleware/error")
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const errorMiddleware = require("./middleware/error");
 const app = express();
-
+const helmet = require("helmet");
 // setup body parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // CORS middleware
-app.use(cors())
+app.use(cors());
 
 // Logger middleware
-app.use(logger("dev"))
+app.use(logger("dev"));
 
-
-
+// Basic security middleware
+app.use(helmet());
 
 // Setup Product route
-app.use("/api/v1", require("./routes/products.routes"))
-
+app.use("/api/v1", require("./routes/products.routes"));
 
 // Error handling middleware
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
