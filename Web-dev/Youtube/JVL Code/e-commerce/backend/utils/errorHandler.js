d0a9a1d2cd5b9df3a9aead5f63907377cd99ce86
mr-nishanth <innocentnishanth@gmail.com>
2023-02-21 22:43:56 +0530

    update stackTrace ErrorHandler to Error class

--- Web-dev/Youtube/JVL Code/e-commerce/backend/utils/errorHandler.js
+++ Web-dev/Youtube/JVL Code/e-commerce/backend/utils/errorHandler.js
@@ -3,7 +3,7 @@ class ErrorHandler extends Error {
     // Calling parent constructor for set new error message
     super(message);
     this.statusCode = statusCode;
-    ErrorHandler.captureStackTrace(this, this.constructor); // captureStackTrace is a method of Error class which is used to capture the stack trace of the error and set it to the stack property of the error object which is used to print the stack trace of the error.
+    Error.captureStackTrace(this, this.constructor); // captureStackTrace is a method of Error class which is used to capture the stack trace of the error and set it to the stack property of the error object which is used to print the stack trace of the error.
   }
 }
 
