12d5ca5e1c1e2086a680da016f69ccc3cc89bf17
mr-nishanth <innocentnishanth@gmail.com>
2023-02-28 19:31:24 +0530

    [A] rateLimit

--- Web-dev/Youtube/ChaooCharles/chat-app/server/index.js
+++ Web-dev/Youtube/ChaooCharles/chat-app/server/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const rateLimit = require("express-rate-limit");
 const app = express();
 
 app.use(cors());
@@ -18,6 +19,16 @@ app.use(morgan("dev"));
 const PORT = process.env.PORT || 8000;
 
 app.disable("x-powered-by");
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 25, // Limit each IP to 25 requests per `window` (here, per 5 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 // ROUTES
 app.use("/api/users", require("./routes/user.routes"));
