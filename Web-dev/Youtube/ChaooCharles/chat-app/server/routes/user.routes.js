b6204ed36425cbfbd5ee47bf73cc35245088ec31
mr-nishanth <innocentnishanth@gmail.com>
2023-02-28 17:46:58 +0530

    [A] find user route

--- Web-dev/Youtube/ChaooCharles/chat-app/server/routes/user.routes.js
+++ Web-dev/Youtube/ChaooCharles/chat-app/server/routes/user.routes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 
-const { registerUser, loginUser } = require("../controllers/user.controllers");
+const {
+  registerUser,
+  loginUser,
+  getUserById,
+} = require("../controllers/user.controllers");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/find/:userId", getUserById);
 
 module.exports = router;
