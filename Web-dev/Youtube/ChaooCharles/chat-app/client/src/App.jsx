c4e2c7f8c29642938c5683626b7fe1e55d0581be
mr-nishanth <innocentnishanth@gmail.com>
2023-02-28 18:22:42 +0530

    Setup router

--- Web-dev/Youtube/ChaooCharles/chat-app/client/src/App.jsx
+++ Web-dev/Youtube/ChaooCharles/chat-app/client/src/App.jsx
@@ -1,4 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 const App = () => {
-  return <>Chat Application</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </>
+  );
 };
 export default App;
