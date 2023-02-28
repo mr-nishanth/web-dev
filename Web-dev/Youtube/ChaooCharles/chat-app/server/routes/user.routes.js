7ab174b27912f83b0925d0ade10e32751a219c84
mr-nishanth <innocentnishanth@gmail.com>
2023-02-28 18:05:55 +0530

    [A] getAllUsers

--- Web-dev/Youtube/ChaooCharles/chat-app/server/routes/user.routes.js
+++ Web-dev/Youtube/ChaooCharles/chat-app/server/routes/user.routes.js
@@ -4,10 +4,12 @@ const {
   registerUser,
   loginUser,
   getUserById,
+  getAllUsers,
 } = require("../controllers/user.controllers");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", getUserById);
+router.get("/", getAllUsers);
 
 module.exports = router;
