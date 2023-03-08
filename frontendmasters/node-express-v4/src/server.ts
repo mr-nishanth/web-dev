53674f965ec249e2a7e4893a88777a81586b3866
mr-nishanth <innocentnishanth@gmail.com>
2023-03-08 19:55:03 +0530

    [A] Common Package

--- frontendmasters/node-express-v4/src/server.ts
+++ frontendmasters/node-express-v4/src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import router from "./router";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/api", router);
 
 export default app;
