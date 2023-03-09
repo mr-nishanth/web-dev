c22791febd79bee09a378639afd12724e55646ae
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 11:24:58 +0530

    [A] Career Layout

---
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/Layouts/CareersLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+const CareersLayout = () => {
+  return (
+    <div className="careers-layout">
+      <h2>Careers</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
+        accusantium.
+      </p>
+      <Outlet />
+    </div>
+  );
+};
+export default CareersLayout;
