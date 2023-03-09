4c081f498d83baad209c01d7df90ad7602b8e6cc
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 19:19:34 +0530

    [A] Dashboard

--- Web-dev/Youtube/TheNetNinja/budget-app/src/App.jsx
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/App.jsx
@@ -1,5 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+  },
+  {
+    path: "/about",
+    element: <Dashboard />,
+  },
+]);
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
