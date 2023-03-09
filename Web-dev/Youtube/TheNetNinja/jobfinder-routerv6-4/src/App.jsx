1869c3bf509695f04cf40419ee3b26aee89186aa
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 10:53:34 +0530

    [A] help layout

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
@@ -4,6 +4,9 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import Contact from "./components/help/Contact";
+import Faq from "./components/help/Faq";
+import HelpLayout from "./Layouts/HelpLayout";
 import RootLayout from "./Layouts/RootLayout";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -13,6 +16,11 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        {/* /help/faq */}
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>
   )
 );
