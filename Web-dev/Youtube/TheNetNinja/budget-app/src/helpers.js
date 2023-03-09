ad4bf03c048f7bfcf0cf7453bcdae44fd1c21a80
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 19:55:32 +0530

    [A]  Delete Item

--- Web-dev/Youtube/TheNetNinja/budget-app/src/helpers.js
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/helpers.js
@@ -1,5 +1,9 @@
 // Local storage
-
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
+};
+
+// Delete Item
+export const deleteItem = ({ key }) => {
+  return localStorage.removeItem(key);
 };
