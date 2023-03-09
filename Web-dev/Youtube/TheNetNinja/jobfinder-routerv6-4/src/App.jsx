f487560c05eb8460bba99a4febadcdd1386b1abb
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 11:25:36 +0530

    [A] Career Layout

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
@@ -4,10 +4,13 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import Careers from "./components/careers/Careers";
 import Contact from "./components/help/Contact";
 import Faq from "./components/help/Faq";
+import CareersLayout from "./Layouts/CareersLayout";
 import HelpLayout from "./Layouts/HelpLayout";
 import RootLayout from "./Layouts/RootLayout";
+import { careersLoader } from "./loaders/careersLoader";
 import About from "./pages/About";
 import NotFound from "./pages/error/NotFound";
 import Home from "./pages/Home";
@@ -17,11 +20,17 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
       <Route path="help" element={<HelpLayout />}>
         {/* /help/faq */}
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index loader={careersLoader} element={<Careers />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Route>
   )
