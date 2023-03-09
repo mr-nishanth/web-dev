48f9ba17fda09eeffcc3e04586201979a6f71495
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 10:16:28 +0530

    [A] New Route

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/App.jsx
@@ -1,5 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./Layouts/RootLayout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <div className="App">React-router-dom</div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
