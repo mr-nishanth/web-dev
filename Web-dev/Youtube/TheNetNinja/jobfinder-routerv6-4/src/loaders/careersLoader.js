7c0885badf1f482ecd8c51463cdaf376049cbe9b
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 11:51:38 +0530

    [A] careerDetailsLoader

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/loaders/careersLoader.js
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/loaders/careersLoader.js
@@ -2,3 +2,9 @@ export const careersLoader = async () => {
   const res = await fetch(`http://localhost:4000/careers`);
   return res.json();
 };
+
+export const careerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+  const res = await fetch(`http://localhost:4000/careers/${id}`);
+  return res.json();
+};
