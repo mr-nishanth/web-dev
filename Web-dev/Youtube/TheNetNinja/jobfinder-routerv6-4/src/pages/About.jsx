ecc051a4f483ccea77d6d0a7ead16f1b88835f37
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 09:27:09 +0530

    [U] Pages

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/pages/About.jsx
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/pages/About.jsx
@@ -8,6 +8,8 @@ const About = () => {
         deserunt porro, assumenda facilis, quasi dicta impedit rerum! Neque
         eius, error corrupti doloribus incidunt modi odit exercitationem illo
         autem dignissimos et omnis ipsa deserunt nulla, quibusdam nobis
+      </p>
+      <p>
         molestias quos saepe fugiat ullam, mollitia debitis ad nam. Cumque earum
         doloribus explicabo aspernatur nihil fugit odit et. Dolor a optio
         tempore distinctio amet quis repudiandae doloremque fugiat consequuntur?
@@ -16,13 +18,6 @@ const About = () => {
         dolorum sapiente corrupti voluptate fugiat.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque
-        corporis molestiae esse, quia sit eaque cumque adipisci voluptatem eos
-        deserunt porro, assumenda facilis, quasi dicta impedit rerum! Neque
-        eius, error corrupti doloribus incidunt modi odit exercitationem illo
-        autem dignissimos et omnis ipsa deserunt nulla, quibusdam nobis
-        molestias quos saepe fugiat ullam, mollitia debitis ad nam. Cumque earum
-        doloribus explicabo aspernatur nihil fugit odit et. Dolor a optio
         tempore distinctio amet quis repudiandae doloremque fugiat consequuntur?
         Nulla nam dignissimos sint nemo neque distinctio, amet quam ex assumenda
         rem facilis voluptates quas, necessitatibus aspernatur tenetur id
