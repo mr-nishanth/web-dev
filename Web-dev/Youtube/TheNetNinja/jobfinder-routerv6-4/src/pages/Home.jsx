83b1e6893ffe72f19d094d76c0c5dd8548fcc8af
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 09:25:56 +0530

    [A] Pages

---
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque
+        corporis molestiae esse, quia sit eaque cumque adipisci voluptatem eos
+        deserunt porro, assumenda facilis, quasi dicta impedit rerum! Neque
+        eius, error corrupti doloribus incidunt modi odit exercitationem illo
+        autem dignissimos et omnis ipsa deserunt nulla, quibusdam nobis
+        molestias quos saepe fugiat ullam, mollitia debitis ad nam. Cumque earum
+        doloribus explicabo aspernatur nihil fugit odit et. Dolor a optio
+        tempore distinctio amet quis repudiandae doloremque fugiat consequuntur?
+        Nulla nam dignissimos sint nemo neque distinctio, amet quam ex assumenda
+        rem facilis voluptates quas, necessitatibus aspernatur tenetur id
+        dolorum sapiente corrupti voluptate fugiat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque
+        corporis molestiae esse, quia sit eaque cumque adipisci voluptatem eos
+        deserunt porro, assumenda facilis, quasi dicta impedit rerum! Neque
+        eius, error corrupti doloribus incidunt modi odit exercitationem illo
+        autem dignissimos et omnis ipsa deserunt nulla, quibusdam nobis
+        molestias quos saepe fugiat ullam, mollitia debitis ad nam. Cumque earum
+        doloribus explicabo aspernatur nihil fugit odit et. Dolor a optio
+        tempore distinctio amet quis repudiandae doloremque fugiat consequuntur?
+        Nulla nam dignissimos sint nemo neque distinctio, amet quam ex assumenda
+        rem facilis voluptates quas, necessitatibus aspernatur tenetur id
+        dolorum sapiente corrupti voluptate fugiat.
+      </p>
+    </div>
+  );
+};
+export default Home;
