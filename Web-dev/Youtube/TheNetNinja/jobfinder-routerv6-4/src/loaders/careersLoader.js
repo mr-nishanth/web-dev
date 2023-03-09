bdab3b45dd0a8ad141009a3241b50a5e60209a73
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 12:03:58 +0530

    [A} Error check

--- Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/loaders/careersLoader.js
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/loaders/careersLoader.js
@@ -1,10 +1,19 @@
 export const careersLoader = async () => {
   const res = await fetch(`http://localhost:4000/careers`);
+  if (!res.ok) {
+    throw new Error("Could not fetch that career");
+  }
+
   return res.json();
 };
 
 export const careerDetailsLoader = async ({ params }) => {
   const { id } = params;
   const res = await fetch(`http://localhost:4000/careers/${id}`);
+
+  if (!res.ok) {
+    throw new Error("Could not find that career");
+  }
+
   return res.json();
 };
