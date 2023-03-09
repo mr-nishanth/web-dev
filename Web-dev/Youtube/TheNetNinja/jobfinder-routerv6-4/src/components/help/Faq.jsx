73795fa22451ba6d17664a9c512f4ba9fa20d0b2
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 10:54:37 +0530

    [A] Faq

---
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/components/help/Faq.jsx
@@ -0,0 +1,74 @@
+const Faq = () => {
+  return (
+    <div className="faq">
+      <h3>Frequently Asked Question</h3>
+      <div className="question">
+        <p>
+          <strong>What about your skill ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>What about your name ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>What about your mobile ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>What about your frontend ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>What about your backend ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>What about your database ?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          inventore aliquam explicabo sapiente laborum eaque sunt non molestiae
+          quae dolorem culpa necessitatibus dolore temporibus dolor, quaerat
+          laudantium et ex fugit!
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Faq;
