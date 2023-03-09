48d87f03f9380a5ffd9834d8c5e8e8fdb70a21d5
mr-nishanth <innocentnishanth@gmail.com>
2023-03-09 10:53:48 +0530

    [A] Help

---
+++ Web-dev/Youtube/TheNetNinja/jobfinder-routerv6-4/src/components/Help.jsx
@@ -0,0 +1,12 @@
+const Help = () => {
+  return (
+    <>
+      <h2>Website Help</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi,
+        quia!
+      </p>
+    </>
+  );
+};
+export default Help;
