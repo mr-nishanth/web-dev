a94ed93a9b2cf879ddb89cd117d2357ba837aec4
mr-nishanth <innocentnishanth@gmail.com>
2023-03-10 19:33:43 +0530

    [A] calculateSpentByBudget

--- Web-dev/Youtube/TheNetNinja/budget-app/src/components/BudgetItem.jsx
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/components/BudgetItem.jsx
@@ -1,7 +1,8 @@
-import { formatCurrency } from "../helpers";
+import { calculateSpentByBudget, formatCurrency } from "../helpers";
 
 const BudgetItem = ({ budget }) => {
   const { id, name, amount, color } = budget;
+  const spent = calculateSpentByBudget(id);
   return (
     <div className="budget">
       <div className="progress-text">
@@ -12,7 +13,7 @@ const BudgetItem = ({ budget }) => {
         {/* percentage */}
       </progress>
       <div className="progress-text">
-        <small>... spent</small>
+        <small>{formatCurrency(spent)} spent</small>
         <small>... remaining</small>
       </div>
     </div>
