3be6d18560f8ab17783383f97c13e4f52ee4928c
mr-nishanth <innocentnishanth@gmail.com>
2023-03-10 21:13:46 +0530

    [A] Delete button

--- Web-dev/Youtube/TheNetNinja/budget-app/src/components/ExpenseItem.jsx
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/components/ExpenseItem.jsx
@@ -1,12 +1,43 @@
-import { formatCurrency, formatDateToLocaleString } from "../helpers";
+import { useFetcher } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import {
+  formatCurrency,
+  formatDateToLocaleString,
+  getAllMatchingItems,
+} from "../helpers";
 
 const ExpenseItem = ({ expense }) => {
+  const fetcher = useFetcher();
   const { name, amount, createdAt } = expense;
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  })[0];
   return (
     <>
       <td>{name}</td>
       <td>{formatCurrency(amount)}</td>
       <td>{formatDateToLocaleString(createdAt)}</td>
+      <td>
+        <Link to={`/budget/${budget.id}`} style={{ "--accent": budget.color }}>
+          {budget.name}
+        </Link>
+      </td>
+      <td>
+        <fetcher.Form method="post">
+          <input type="hidden" name="_action" value="deleteExpense" />
+          <input type="hidden" name="expenseId" value={expense.id} />
+          <button
+            type="submit"
+            className="btn btn--warning"
+            aria-label={`Delete ${expense.name} expense`}
+          >
+            <TrashIcon width={20} />
+          </button>
+        </fetcher.Form>
+      </td>
     </>
   );
 };
