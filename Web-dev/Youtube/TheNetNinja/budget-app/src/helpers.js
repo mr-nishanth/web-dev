e811ba0c2915a33018218b6468883e64b58dd424
mr-nishanth <innocentnishanth@gmail.com>
2023-03-10 21:29:45 +0530

    [Bug Fixed] In deleteItem

--- Web-dev/Youtube/TheNetNinja/budget-app/src/helpers.js
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/helpers.js
@@ -90,7 +90,7 @@ export const deleteItem = ({ key, id }) => {
   const existingData = fetchData(key);
   if (id) {
     const newData = existingData.filter((item) => item.id !== id);
-    return localStorage.setItem(key, JSON, stringify(newData));
+    return localStorage.setItem(key, JSON.stringify(newData));
   }
   return localStorage.removeItem(key);
 };
