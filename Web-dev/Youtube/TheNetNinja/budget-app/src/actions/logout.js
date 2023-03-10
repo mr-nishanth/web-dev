aedde90c8345901b97dfd4817bb347b9c08d6e37
mr-nishanth <innocentnishanth@gmail.com>
2023-03-10 20:01:55 +0530

    [D]  budgets and expenses in local-storage

--- Web-dev/Youtube/TheNetNinja/budget-app/src/actions/logout.js
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/actions/logout.js
@@ -2,9 +2,15 @@ import { redirect } from "react-router-dom";
 import { deleteItem } from "../helpers";
 import { toast } from "react-toastify";
 export async function logoutAction() {
-  // Delete the user
+  // Delete the userName , budgets and expenses
   deleteItem({
     key: "userName",
+  });
+  deleteItem({
+    key: "budgets",
+  });
+  deleteItem({
+    key: "expenses",
   });
   toast.success("You've deleted your account");
   // Redirect the user
