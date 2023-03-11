7c6d05791fef94c19ee11e08d482a05df60cc039
mr-nishanth <innocentnishanth@gmail.com>
2023-03-11 22:47:39 +0530

    [A] App

--- Web-dev/Youtube/JavascriptMastery/portfolio-3d/src/App.jsx
+++ Web-dev/Youtube/JavascriptMastery/portfolio-3d/src/App.jsx
@@ -1,4 +1,39 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          {/* <Hero /> */}
+        </div>
+        {/* <About /> */}
+        {/* <Experience /> */}
+        {/* <Tech /> */}
+        {/* <Works /> */}
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          {/* <Contact /> */}
+          {/* <StarsCanvas /> */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 export default App;
