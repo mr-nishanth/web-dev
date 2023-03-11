1b5eb6350d895690130ced1f574d3eb002bd2ccd
mr-nishanth <innocentnishanth@gmail.com>
2023-03-11 09:38:32 +0530

    [A] Delete Budget and View Details

--- Web-dev/Youtube/TheNetNinja/budget-app/src/components/BudgetItem.jsx
+++ Web-dev/Youtube/TheNetNinja/budget-app/src/components/BudgetItem.jsx
@@ -1,10 +1,14 @@
+import { BanknotesIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+import { Form } from "react-router-dom";
 import {
   calculateSpentByBudget,
   formatCurrency,
   formatPercentage,
 } from "../helpers";
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
   return (
@@ -26,6 +30,35 @@ const BudgetItem = ({ budget }) => {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)} remaining</small>
       </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="POST"
+            action="delete"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Are you sure you want to permanently delete this budget ?"
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit" className="btn">
+              <span>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
