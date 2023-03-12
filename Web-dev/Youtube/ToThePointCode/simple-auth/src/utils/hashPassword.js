b6fc419b97dadbe5155924d5408841cdd2458dd4
mr-nishanth <innocentnishanth@gmail.com>
2023-03-12 20:09:30 +0530

    [A] hashPassword

---
+++ Web-dev/Youtube/ToThePointCode/simple-auth/src/utils/hashPassword.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+export const hashPassword = async (password) => {
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
