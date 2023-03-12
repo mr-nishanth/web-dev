2f19315556b88f02cc7af4ecd22d4734701e818d
mr-nishanth <innocentnishanth@gmail.com>
2023-03-12 19:41:00 +0530

    [A] Start app

---
+++ Web-dev/Youtube/ToThePointCode/simple-auth/src/index.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const { PORT } = process.env;
+
+const startApp = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startApp();
