dc08aa76b8590019f9d9582486a979b4f235786c
mr-nishanth <innocentnishanth@gmail.com>
2023-03-12 19:39:52 +0530

    [A] App

---
+++ Web-dev/Youtube/ToThePointCode/simple-auth/src/app.js
@@ -0,0 +1,16 @@
+require("./config/db");
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+module.exports = app;
