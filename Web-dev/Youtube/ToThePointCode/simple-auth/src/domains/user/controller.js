2e64faea3516c8e026a65e29fe47b099d0acb136
mr-nishanth <innocentnishanth@gmail.com>
2023-03-12 21:10:02 +0530

    [Bug Fixed]

--- Web-dev/Youtube/ToThePointCode/simple-auth/src/domains/user/controller.js
+++ Web-dev/Youtube/ToThePointCode/simple-auth/src/domains/user/controller.js
@@ -1,28 +1,29 @@
 const { hashPassword } = require("../../utils/hashPassword");
 const User = require("./model");
 const createNewUser = async (data) => {
+  // console.log({ data });
   try {
     const { name, email, password } = data;
 
     // Check if user already exists
-    const existingUser = User.findOne({ email });
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       throw new Error("User already exists");
     }
 
     // Hash password
-    const hashPassword = await hashPassword(password);
+    const hashPwd = await hashPassword(password);
     // Create new user
     const newUser = new User({
       name,
       email,
-      password: hashPassword,
+      password: hashPwd,
     });
     // Save user
     const savedUser = await newUser.save();
     return savedUser;
   } catch (error) {
-    throw new Error(error);
+    throw error;
   }
 };
 
