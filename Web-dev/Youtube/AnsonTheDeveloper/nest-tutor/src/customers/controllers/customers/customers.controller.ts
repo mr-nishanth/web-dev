4ac356f4a6d9f499c445500365d325aea53d34d2
mr-nishanth <innocentnishanth@gmail.com>
2023-03-13 21:49:19 +0530

    [A] findCustomerById similar to express

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/controllers/customers/customers.controller.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/controllers/customers/customers.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
-  @Get('')
-  getCustomers() {
-    return this.customersService.findCustomer();
+  @Get(':id')
+  getCustomers(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) {
+      res.send(customer);
+    } else {
+      res.status(404).send({ msg: 'Customer Not Found' });
+    }
   }
 }
--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/services/customers/customers.service.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/services/customers/customers.service.ts
@@ -2,11 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  users = [
+    {
       id: 1,
       email: 'nishanth@gmail.com',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: 'vasee@gmail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'vijay@gmail.com',
+      createdAt: new Date(),
+    },
+  ];
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
