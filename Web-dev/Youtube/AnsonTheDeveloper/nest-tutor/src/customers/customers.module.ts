02fab40e5a427fdb2fdc0a8ad75a7e0d9650fcba
mr-nishanth <innocentnishanth@gmail.com>
2023-03-14 11:59:56 +0530

    [A} Enable Middleware in Different Way

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/customers.module.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/customers.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod } from '@nestjs/common';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { ValidateCustomerMiddleware } from './middlewares/validate-customer.middleware';
 import { CustomersService } from './services/customers/customers.service';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: import('@nestjs/common').MiddlewareConsumer) {
+    //* Method 1
+    // consumer.apply(ValidateCustomerMiddleware).forRoutes(CustomersController) // All routes
+
+    //* Method 3 All routes exclude some paths
+    // consumer.apply(ValidateCustomerMiddleware).exclude({
+    //  path:'api/customers/create',
+    //  method: RequestMethod.POST,
+    // }).forRoutes(CustomersController)
+
+    //* Method 2
+    consumer.apply(ValidateCustomerMiddleware).forRoutes(
+      {
+        path: 'customers/search/:id',
+        method: RequestMethod.GET,
+      },
+      {
+        path: 'customers/:id',
+        method: RequestMethod.GET,
+      },
+    );
+  }
+}
