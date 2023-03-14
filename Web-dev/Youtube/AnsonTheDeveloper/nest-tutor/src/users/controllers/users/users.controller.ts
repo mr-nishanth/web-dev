8d6ce4f1bd545bf67a621461f48dd91a3772074b
mr-nishanth <innocentnishanth@gmail.com>
2023-03-14 11:24:22 +0530

    [A] Filtering Passwords method 2

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/users/controllers/users/users.controller.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/users/controllers/users/users.controller.ts
@@ -1,13 +1,15 @@
 import {
+  ClassSerializerInterceptor,
   Controller,
   Get,
   HttpException,
   HttpStatus,
   Inject,
   Param,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from 'src/users/services/users/users.service';
-import { User } from 'src/users/types';
+import { SerializedUser, User } from 'src/users/types';
 
 @Controller('users')
 export class UsersController {
@@ -20,12 +22,13 @@ export class UsersController {
     return this.usersService.getUsers();
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':username')
   getByUsername(@Param('username') username: string): User {
     const user = this.usersService.getUserByUsername(username);
     if (!user) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
-    return user;
+    return new SerializedUser(user);
   }
 }
--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/users/types/index.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/users/types/index.ts
@@ -11,4 +11,8 @@ export class SerializedUser {
 
   @Exclude()
   password: string;
+
+  constructor(partial: Partial<SerializedUser>) {
+    Object.assign(this, partial);
+  }
 }
