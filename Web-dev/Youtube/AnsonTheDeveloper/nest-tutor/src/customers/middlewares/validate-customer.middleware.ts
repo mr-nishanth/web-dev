139086f0ca1da8c1e0ad25d1669977fe4a7bbc23
mr-nishanth <innocentnishanth@gmail.com>
2023-03-14 12:11:46 +0530

    [A] Simple Middleware

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/middlewares/validate-customer.middleware.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/middlewares/validate-customer.middleware.ts
@@ -1,11 +1,28 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class ValidateCustomerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`ValidateCustomerMiddleware`);
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Unauthorized, No Authorized Token Provide' });
+    }
+
+    if (!(authorization !== 'hell')) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Unauthorized, Invalid Authorized Token Provide' });
+    }
     next();
   }
 }
