64d63436e61e4114ab4c1dcfdab78a097a6841ab
mr-nishanth <innocentnishanth@gmail.com>
2023-03-14 10:42:49 +0530

    [A] ValidateNested Objects

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/dtos/CreateCustomer.dto.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/customers/dtos/CreateCustomer.dto.ts
@@ -1,11 +1,26 @@
 /* eslint-disable prettier/prettier */
-import { IsEmail, IsNotEmpty, IsNumberString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsNumberString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateAddressDto } from './CreateAddress.dto';
 export class CreateCustomerDto {
   @IsNumberString()
   @IsNotEmpty()
   id: number;
+
   @IsEmail()
   email: string;
+
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmptyObject() // If address is optional [remove @IsNotEmptyObject()]
+  @ValidateNested()
+  @Type(() => CreateAddressDto)
+  address: CreateAddressDto;
 }
