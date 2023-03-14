4e86faa0192346a09b0c8a3af845dad43d923580
mr-nishanth <innocentnishanth@gmail.com>
2023-03-14 15:57:27 +0530

    [A] Comment for email

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/typeorm/User.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/typeorm/User.ts
@@ -9,8 +9,11 @@ export class User {
   @Column({ nullable: false, default: '' })
   username: string;
 
-  @Column({ name: 'email_address', nullable: false, default: '' })
-  emailAddress: string;
+  // @Column({ name: 'email_address', nullable: false, default: '' })
+  // emailAddress: string;
+
+  @Column({ nullable: false, default: '' })
+  email: string;
 
   @Column({ nullable: false, default: '' })
   password: string;
