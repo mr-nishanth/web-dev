cb3faf031d2a8b9fe1ea8668d14301f0d6e592db
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:36:00 +0530

    [A]  @UseGuards(AuthGuard('local')) passport way

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto';
 
@@ -7,12 +8,19 @@ import { LoginDto } from './dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // @UseGuards(AuthGuard('local'))
+  // @Post('login')
+  // async login(@Body() loginDto: LoginDto) {
+  //   return await this.authService.validateUser(
+  //     loginDto.email,
+  //     loginDto.password,
+  //   );
+  // }
+
+  // Use Passport way
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    return await this.authService.validateUser(
-      loginDto.email,
-      loginDto.password,
-    );
+  async login(@Req() req: Request) {
+    return req.user;
   }
 }
