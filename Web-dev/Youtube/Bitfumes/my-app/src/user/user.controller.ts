f8e6073005a2b63402570d69b0f3aa7bc71413d7
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 11:44:42 +0530

    [A] UserController

--- Web-dev/Youtube/Bitfumes/my-app/src/user/user.controller.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/user/user.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getUsers() {
+    return { name: 'Nishanth', email: 'nishanth@gmail.com' };
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
+  }
+}
