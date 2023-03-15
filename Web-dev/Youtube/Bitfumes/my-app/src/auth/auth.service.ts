269403de1cab3bf08358c16a061c8a2cbc4fdcea
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:35:27 +0530

    [R] whistle space

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
@@ -7,7 +7,7 @@ export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.userService.findUserByEmail(email);    
+    const user = await this.userService.findUserByEmail(email);
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
