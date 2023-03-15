ac75da4e02bd790183cc6fbda01d85432b6de8ce
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:31:44 +0530

    [A] import PassportModule and LocalStrategy

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.module.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 @Module({
   controllers: [AuthController],
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
