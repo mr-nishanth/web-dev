69946414323d6eb71fc3da04ffe866750bf50bef
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:31:14 +0530

    [A] UseGuards

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
@@ -1,16 +1,13 @@
-import {
-  Body,
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.validateUser(
