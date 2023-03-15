2f1c4ea85c4908329d3c9d246d0f81224dbe8d0f
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 14:44:45 +0530

    [A] TypeOrmModule for setup db connection

--- Web-dev/Youtube/Bitfumes/my-app/src/app.module.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
 @Module({
   controllers: [AppController],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_tutor',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
