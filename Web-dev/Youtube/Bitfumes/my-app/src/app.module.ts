9897bd0294faa2a03c4b1707d32af85b200a8036
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 11:37:57 +0530

    [U]  Renamed user to _user

--- Web-dev/Youtube/Bitfumes/my-app/src/app.module.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AppController],
-  imports: [UserModule],
 })
 export class AppModule {}
