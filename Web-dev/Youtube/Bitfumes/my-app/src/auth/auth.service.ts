df569d184b073b2e17f72ff151b833c0775d5abf
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:15:42 +0530

    [A] AuthService

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // Dependency Injection
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findUserByEmail(email);
+    console.log({ user });
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
