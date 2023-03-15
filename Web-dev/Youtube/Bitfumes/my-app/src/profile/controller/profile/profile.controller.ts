05d80ba70e9a6dd2e41329262ae651e417a88312
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 17:03:57 +0530

    [A] @UseGuards(AuthGuard('jwt'))

--- Web-dev/Youtube/Bitfumes/my-app/src/profile/controller/profile/profile.controller.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/profile/controller/profile/profile.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ProfileService } from 'src/profile/service/profile/profile.service';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   profile() {
     return this.profileService.profile();
