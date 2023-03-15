42d0e2eba180d757a40ac71cf906b006cf423c42
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 09:21:01 +0530

    [A] Incomplete

--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/auth/controllers/auth/auth.controller.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('local'))
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  async login(@Request() req) {}
+}
--- Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/auth/services/auth/auth.service.ts
+++ Web-dev/Youtube/AnsonTheDeveloper/nest-tutor/src/auth/services/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // Inject User service
+  constructor(
+    @Inject('USER_SERVICE') private readonly userService: UsersService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const userDB = await this.userService.findUserByUsername(username);
+    if (userDB) {
+      // Check password
+      console.log({ userDB });
+    }
+  }
+}
