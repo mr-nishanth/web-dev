19db69f2b1dba549a3c282e85dd56c5adc994800
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:46:13 +0530

    [A] Register JwtModule

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.module.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 @Module({
   controllers: [AuthController],
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
