ffd42a6cdd47b12628fae547121546323969170e
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 16:47:16 +0530

    [A] login service for jwt

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   // Dependency Injection
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findUserByEmail(email);
@@ -13,5 +17,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      email: user.email,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
