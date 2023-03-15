27d89fce17a65f38b7a905b93f208576529c0124
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 15:53:15 +0530

    [A] Login

--- Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller, 
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // Dependency Injection
+  constructor(private readonly userService: UserService) {}
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.userService.findUserByEmail(loginDto.email);
+    console.log({ user });
+    if (!user) {
+      throw new HttpException('UNAUTHORIZED Email', HttpStatus.UNAUTHORIZED);
+    }
+    if (user.password !== loginDto.password) {
+      throw new HttpException('UNAUTHORIZED Password', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+}
