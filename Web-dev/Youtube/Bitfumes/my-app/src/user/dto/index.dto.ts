5cbf89eec604e2ba7bc3c50c3895e61d923a34dd
mr-nishanth <innocentnishanth@gmail.com>
2023-03-15 12:49:26 +0530

    [A] class-validator

--- Web-dev/Youtube/Bitfumes/my-app/src/user/dto/index.dto.ts
+++ Web-dev/Youtube/Bitfumes/my-app/src/user/dto/index.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
 export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
