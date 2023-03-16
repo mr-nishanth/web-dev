f03bf93089a8189d8268d62b538ce46e97ae6393
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 11:08:35 +0530

    [A] Config ProductSchema

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.module.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ProductSchema } from './schema/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
