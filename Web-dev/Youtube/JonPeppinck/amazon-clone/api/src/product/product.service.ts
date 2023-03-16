6ab9854d712c9eafe79baba13526cbef4fc58772
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 11:34:39 +0530

    [A] Create Product

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.service.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductDocument } from './schema/product.schema';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product')
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<ProductDocument> {
+    const newProduct = new this.productModel(createProductDto);
+    return newProduct.save();
+  }
+}
