66302eaa9dba77b7d08d85b1596234e49f77c6e1
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 15:59:49 +0530

    [A] Validation

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/dto/index.dto.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/dto/index.dto.ts
@@ -1,15 +1,33 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
 
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   description?: string;
 }
 
 export class UpdateProductDto {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   name?: string;
 
+  @IsOptional()
+  @IsNotEmpty()
+  @IsNumber()
   price?: number;
 
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
   description?: string;
 }
