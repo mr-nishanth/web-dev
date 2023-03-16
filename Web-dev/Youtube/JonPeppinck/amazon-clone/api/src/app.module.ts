b5feaa3b1277908107722a5b7a0266c9e405d694
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 10:46:40 +0530

    [A] MongooseModule

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/app.module.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/app.module.ts
@@ -1,10 +1,15 @@
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
