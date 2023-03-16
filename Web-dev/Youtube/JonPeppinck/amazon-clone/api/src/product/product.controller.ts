410ef18d1a2f7469e6e9839a73a6f9cf3ba0b158
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 11:59:25 +0530

    [A] CRUD ProductController

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.controller.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateProductDto, UpdateProductDto } from './dto/index.dto';
+import { ProductService } from './product.service';
+import { ProductDocument } from './schema/product.schema';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Post()
+  createProduct(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<ProductDocument> {
+    return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  findAllProducts(): Promise<ProductDocument[]> {
+    return this.productService.findAllProducts();
+  }
+
+  @Get(':id')
+  findOneProduct(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.findOneProduct(id);
+  }
+
+  @Patch(':id')
+  findProductAndUpdate(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ): Promise<ProductDocument> {
+    return this.productService.updateProduct(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  findProductAndDelete(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.deleteProduct(id);
+  }
+}
