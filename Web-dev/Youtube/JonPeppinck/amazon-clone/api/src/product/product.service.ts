9f21080b954ca945cbbe1e4f712afd74488bf42e
mr-nishanth <innocentnishanth@gmail.com>
2023-03-16 11:59:06 +0530

    [A] CRUD ProductService

--- Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.service.ts
+++ Web-dev/Youtube/JonPeppinck/amazon-clone/api/src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateProductDto } from './dto/create-product.dto';
+import { CreateProductDto, UpdateProductDto } from './dto/index.dto';
 import { ProductDocument } from './schema/product.schema';
 
 @Injectable()
@@ -14,5 +14,26 @@ export class ProductService {
   async create(createProductDto: CreateProductDto): Promise<ProductDocument> {
     const newProduct = new this.productModel(createProductDto);
     return newProduct.save();
+  }
+
+  async findAllProducts(): Promise<ProductDocument[]> {
+    return this.productModel.find().exec();
+  }
+
+  async findOneProduct(id: string): Promise<ProductDocument> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async updateProduct(
+    id: string,
+    updateProductDto: UpdateProductDto,
+  ): Promise<ProductDocument> {
+    return this.productModel
+      .findByIdAndUpdate(id, updateProductDto, { new: true })
+      .exec();
+  }
+
+  async deleteProduct(id: string): Promise<ProductDocument> {
+    return this.productModel.findByIdAndDelete(id).exec();
   }
 }
