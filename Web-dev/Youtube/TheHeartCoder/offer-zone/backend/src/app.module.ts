fafd1206b36f57dc2ff51af1b0094aea44407bdf
mr-nishanth <innocentnishanth@gmail.com>
2023-03-17 15:16:17 +0530

    [A] MongooseModule

--- Web-dev/Youtube/TheHeartCoder/offer-zone/backend/src/app.module.ts
+++ Web-dev/Youtube/TheHeartCoder/offer-zone/backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import config from 'config';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.get('mongoDbUrl'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      w: 1,
+      keepAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
