ba0fc3c46f8213d819713ffcf82ea22fecccb61e
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 16:32:37 +0530

    [A]Incremental Static Regeneration revalidate(60s)

--- Web-dev/Youtube/Dave Gray/learn-nextjs/lib/getUserPost.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/lib/getUserPost.tsx
@@ -1,7 +1,14 @@
 const getUserPost = async (userId: string) => {
+  // { cache: "force-cache" } by default cache
+  // { cache: "no-store" } never cache the data because always dynamical
+
+  // { next: { revalidate: 60sec } }
+  // ISR Incremental Static Regeneration
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    { next: { revalidate: 60 } }
   );
+
   if (!res.ok) throw new Error("Failed to get all users");
   return res.json();
 };
