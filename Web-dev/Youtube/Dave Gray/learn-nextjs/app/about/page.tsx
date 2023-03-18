f392686336b7305e798053139b0daa108dadd6b8
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 08:18:51 +0530

    [A] About

---
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <Link href={"/"}>Back to homepage</Link>
+    </>
+  );
+};
+
+export default About;
