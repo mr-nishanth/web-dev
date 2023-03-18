9e4c716ca947517ede7c03034d7fe5becc7ab2bc
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 18:16:25 +0530

    [R] Link

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/page.tsx
@@ -1,17 +1,7 @@
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Learn Nextjs</h1>
-      <p>
-        <Link href={"/about"}>Back to About page</Link> <br /> <br />
-        <Link href={"/users"}>Back to User page</Link>
-      </p>
-    </main>
-  );
+  return <main className={inter.className}></main>;
 }
