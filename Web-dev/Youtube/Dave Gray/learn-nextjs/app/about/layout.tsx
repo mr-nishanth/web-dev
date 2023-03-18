0876d42556745f468d45c8d483c2cee16bdc9e45
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 08:28:21 +0530

    [A] Loading

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/about/layout.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/about/layout.tsx
@@ -1,5 +1,10 @@
 import styles from "./styles.module.css";
 
+export const metadata = {
+  title: "About Page",
+  description: "About Page Description",
+};
+
 export default function AboutLayout({
   children,
 }: {
--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/about/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/about/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 const About = () => {
+  // throw new Error("Not implemented");
   return (
     <>
       <h1>About</h1>
