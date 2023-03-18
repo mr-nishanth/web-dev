e2300523e73637374c7582c88b745a79473a1baa
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 08:42:36 +0530

    [A] User

---
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Users",
+  description: "User description",
+};
+
+const Users = () => {
+  return <div>Users</div>;
+};
+
+export default Users;
