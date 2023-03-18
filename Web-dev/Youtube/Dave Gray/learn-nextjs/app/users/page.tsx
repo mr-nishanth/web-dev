5a05ae4769e1c1d9fbde90250a4f5ef9ba7ffe2a
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 08:58:41 +0530

    [A] User content

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/page.tsx
@@ -1,12 +1,38 @@
+import getAllUsers from "@/lib/getAllUsers";
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Users",
   description: "User description",
 };
 
-const Users = () => {
-  return <div>Users</div>;
+const Users = async () => {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users = await usersData;
+
+  console.log("Hello");
+
+  const content = (
+    <section>
+      <h2>
+        <Link href={"/"}>Back to Home</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+
+  return <div>{content}</div>;
 };
 
 export default Users;
