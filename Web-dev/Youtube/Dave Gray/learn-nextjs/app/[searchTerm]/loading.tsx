6f7b090ffe486762d3a257b1eb74392d9f7fb636
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 18:22:49 +0530

    [A] Loading

---
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/[searchTerm]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <main className='bg-slate-200 mx-auto max-w-lg p-1 min-h-screen'>
+      <h2 className='m-4 text-2xl font-bold'>Loading...</h2>;
+    </main>
+  );
+};
+
+export default loading;
