f1a9f2972293a42630e17a023ce4c51c39e31894
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 17:16:41 +0530

    [A] Navbar

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/layout.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/layout.tsx
@@ -2,11 +2,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next",
-  description: "Learn Nestjs",
+  title: "Black Rockets",
+  description: "Pursuing your Black Rockets",
 };
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className='bg-slate-800'>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
