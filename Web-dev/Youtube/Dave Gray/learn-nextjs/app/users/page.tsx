e23d08600746267dcda4af5e8643e5709496ac11
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 17:01:30 +0530

    [A} 404 check

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/page.tsx
@@ -1,6 +1,7 @@
 import getAllUsers from "@/lib/getAllUsers";
 import type { Metadata } from "next";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Users",
@@ -10,25 +11,25 @@ export const metadata: Metadata = {
 const Users = async () => {
   const usersData: Promise<User[]> = getAllUsers();
   const users = await usersData;
-
-  console.log("Hello");
-
+  // console.log(users);
+  if (users.length === 0) return notFound();
   const content = (
     <section>
       <h2>
         <Link href={"/"}>Back to Home</Link>
       </h2>
       <br />
-      {users.map((user) => {
-        return (
-          <>
-            <p key={user.id}>
-              <Link href={`/users/${user.id}`}>{user.name}</Link>
-            </p>
-            <br />
-          </>
-        );
-      })}
+      {users &&
+        users?.map((user) => {
+          return (
+            <>
+              <p key={user.id}>
+                <Link href={`/users/${user.id}`}>{user.name}</Link>
+              </p>
+              <br />
+            </>
+          );
+        })}
     </section>
   );
 
