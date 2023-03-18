64da24ec7506032472676ffdb495f17800b78a12
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 16:01:33 +0530

    [A] Way 2 fetching

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/[userId]/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/[userId]/page.tsx
@@ -1,6 +1,7 @@
 import getUser from "@/lib/getUser";
 import getUserPost from "@/lib/getUserPost";
-import React from "react";
+import React, { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
 
 type Params = {
   params: {
@@ -14,12 +15,18 @@ const User = async ({ params: { userId } }: Params) => {
   const userPostData: Promise<Post[]> = getUserPost(userId);
 
   // WAY 1
-  const [user, userPost] = await Promise.all([userData, userPostData]);
+  // const [user, userPost] = await Promise.all([userData, userPostData]);
+
+  const user = await userData;
 
   return (
     <>
       <h2>{user.name}</h2>
       <br />
+      <Suspense fallback={<h2>Loading... from suspense</h2>}>
+        {/* @ts-expect-error Async Server Component */}
+        <UserPosts promise={userPostData} />
+      </Suspense>
     </>
   );
 };
