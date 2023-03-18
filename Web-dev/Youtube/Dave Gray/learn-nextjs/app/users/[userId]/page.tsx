47afc8fcb4fce7e2846ef4a45966181c0f2eac64
mr-nishanth <innocentnishanth@gmail.com>
2023-03-18 16:09:17 +0530

    [A] generateMetadata

--- Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/[userId]/page.tsx
+++ Web-dev/Youtube/Dave Gray/learn-nextjs/app/users/[userId]/page.tsx
@@ -2,12 +2,25 @@ import getUser from "@/lib/getUser";
 import getUserPost from "@/lib/getUserPost";
 import React, { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 
 type Params = {
   params: {
     userId: string;
   };
 };
+
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
 
 const User = async ({ params: { userId } }: Params) => {
   // NOTE : await not using because here parallel request
