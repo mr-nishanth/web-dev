4053c5a1100b2ab7286fba7fe711870df5d5b848
mr-nishanth <innocentnishanth@gmail.com>
2023-03-20 15:21:50 +0530

    [A] Wrapper

--- Web-dev/Youtube/KishanSheth/pokemon-app/src/pages/Pokemon.tsx
+++ Web-dev/Youtube/KishanSheth/pokemon-app/src/pages/Pokemon.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Wrapper from "../sections/Wrapper";
 
 const Pokemon = () => {
   return <div>Pokemon</div>;
 };
 
-export default Pokemon;
+export default Wrapper(Pokemon);
--- Web-dev/Youtube/KishanSheth/pokemon-app/src/pages/Search.tsx
+++ Web-dev/Youtube/KishanSheth/pokemon-app/src/pages/Search.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+// @ts-nocheck
 
-const Search = () => {
-  return <div>Search</div>;
-};
+import React, { useEffect } from "react";
+import Wrapper from "../sections/Wrapper";
+import { debounce } from "../utils";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getInitialPokemonData } from "../app/reducers/getInitialPokemonData";
+import { getPokemonsData } from "../app/reducers/getPokemonsData";
+import Loader from "../components/Loader";
+import { setLoading } from "../app/slices/AppSlice";
 
-export default Search;
+import PokemonCardGrid from "../components/PokemonCardGrid";
+
+function Search() {
+  const handleChange = debounce((value: string) => getPokemon(value), 300);
+  const isLoading = useAppSelector(({ app: { isLoading } }) => isLoading);
+
+  const dispatch = useAppDispatch();
+  const { allPokemon, randomPokemons } = useAppSelector(
+    ({ pokemon }) => pokemon
+  );
+
+  useEffect(() => {
+    dispatch(getInitialPokemonData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (allPokemon) {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonsData(randomPokemonsId));
+    }
+  }, [allPokemon, dispatch]);
+
+  useEffect(() => {
+    if (randomPokemons) {
+      dispatch(setLoading(false));
+    }
+  }, [randomPokemons, dispatch]);
+
+  const getPokemon = async (value: string) => {
+    if (value.length) {
+      const pokemons = allPokemon.filter((pokemon) =>
+        pokemon.name.includes(value.toLowerCase())
+      );
+      dispatch(getPokemonsData(pokemons));
+    } else {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonsData(randomPokemonsId));
+    }
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="search">
+          <input
+            type="text"
+            onChange={(e) => handleChange(e.target.value)}
+            className="pokemon-searchbar"
+            placeholder="Search Pokemon"
+          />
+          <PokemonCardGrid pokemons={randomPokemons} />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Wrapper(Search);
