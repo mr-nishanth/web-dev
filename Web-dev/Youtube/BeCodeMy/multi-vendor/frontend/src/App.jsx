530a8fe4b073fa55980497eaf45e2ed98b8e7d22
mr-nishanth <innocentnishanth@gmail.com>
2023-03-24 09:31:19 +0530

    [A] Route for LoginPage

--- Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import {BrowserRouter as Router ,Routes , Route} from "react-router-dom"
+import {LoginPage} from "./Routes.js"
 const App = () => {
-  return <div className="">App</div>;
+  return <div className="">
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+        
+      </Routes>
+    </Router>
+  </div>;
 };
 
 export default App;
