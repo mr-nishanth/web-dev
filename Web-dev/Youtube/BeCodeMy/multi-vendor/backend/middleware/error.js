e9cce8fe303ba16b530105551794ed6d6e2a5721
mr-nishanth <innocentnishanth@gmail.com>
2023-03-24 12:13:15 +0530

    [A] Error

---
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/backend/middleware/error.js
@@ -0,0 +1,35 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+
+module.exports = (err, res, req, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  //   Wrong mongodb ID
+  if (err.name === "CastError") {
+    const message = `Resource not found with mongodb ID , Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Duplicate Key (eg: email address)
+  if (err.code === 11000) {
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //   Wrong jwt token/Invalid jwt token
+  if (err.name === "JsonWebTokenError") {
+    const message = `Your url is invalid, please try again letter`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Expired Jwt token
+  if (err.name === "TokenExpireError") {
+    const message = `Your url is expired, please try again letter`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.statusCode(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
