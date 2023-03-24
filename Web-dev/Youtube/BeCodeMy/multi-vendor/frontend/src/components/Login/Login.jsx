019512ee9c86aed6418416d8a57d7d8ae55c88ce
mr-nishanth <innocentnishanth@gmail.com>
2023-03-24 10:20:50 +0530

    [A] [U] bg-gray-50

--- Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/components/Login/Login.jsx
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [passVisible, setPassVisible] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Login to your account
