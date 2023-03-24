2edd40a43207baa7fb189fc7d6d3c41a119556e9
mr-nishanth <innocentnishanth@gmail.com>
2023-03-24 10:24:26 +0530

    [A] SignUp

--- Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter as Router ,Routes , Route} from "react-router-dom"
+import SignUpPage from "./pages/SignUpPage.jsx";
 import {LoginPage} from "./Routes.js"
 const App = () => {
   return <div className="">
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage/>}/>                
+        <Route path="/sign-up" element={<SignUpPage/>}/>                
       </Routes>
     </Router>
   </div>;
---
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SignUp from "../components/SignUp/SignUp.jsx"
+const SignUpPage = () => {
+  return <div>
+    <SignUp />
+  </div>;
+};
+
+export default SignUpPage;
