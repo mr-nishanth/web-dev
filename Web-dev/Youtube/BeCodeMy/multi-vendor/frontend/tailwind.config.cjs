fcaaa5550f57ce503b961f0d4e5029c2edee06a4
mr-nishanth <innocentnishanth@gmail.com>
2023-03-24 08:56:18 +0530

    [A] Initial Setup

---
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    fontFamily:{
+        Roboto:["Roboto","sans-serif"],
+        Poppins:["Poppins","sans-serif"]
+    },
+    extend: {
+      screens:{
+        "1000px": "1050px",
+        "1100px":"1110px",
+        "1300px": "1300px",
+        "800px": "800px",
+        "400px": "400px",
+      }
+    },
+  },
+  plugins: [],
+}
