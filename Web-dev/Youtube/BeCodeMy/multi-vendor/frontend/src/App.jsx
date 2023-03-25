0406f57eb63f4c28908960b9600e0c28b332e959
mr-nishanth <innocentnishanth@gmail.com>
2023-03-25 13:42:01 +0530

    [A] ActivationPage route and toast configuration

--- Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/frontend/src/App.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import {BrowserRouter as Router ,Routes , Route} from "react-router-dom"
-import {LoginPage,SignUpPage} from "./Routes.js"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginPage, SignUpPage, ActivationPage } from "./Routes.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  return <div className="">
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage/>}/>                
-        <Route path="/sign-up" element={<SignUpPage/>}/>                
-      </Routes>
-    </Router>
-  </div>;
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route
+            path="/activation/:activationToken"
+            element={<ActivationPage />}
+          />
+        </Routes>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
