05ee3a962802f0a76cd86adc17ea4d9a4bcbc2ea
mr-nishanth <innocentnishanth@gmail.com>
2023-03-25 14:17:40 +0530

    [A] isAuthenticated

--- Web-dev/Youtube/BeCodeMy/multi-vendor/backend/middleware/auth.js
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/backend/middleware/auth.js
@@ -1,3 +1,13 @@
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncError = require("./catchAsyncError");
 const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+exports.isAuthenticated = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) return next(new ErrorHandler("Please login to continue", 401));
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id).learn().exec();
+  next();
+});
