109d082f050fe502e06257b9f74d84653541453b
mr-nishanth <innocentnishanth@gmail.com>
2023-03-25 13:59:11 +0530

    [U] Set Password Field to undefined

--- Web-dev/Youtube/BeCodeMy/multi-vendor/backend/utils/jwtToken.js
+++ Web-dev/Youtube/BeCodeMy/multi-vendor/backend/utils/jwtToken.js
@@ -2,6 +2,9 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
+  // Set password to undefined
+  user.password = undefined;
+
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000), // 90 days
