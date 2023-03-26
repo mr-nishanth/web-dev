2e78887e6b6ba30c624807c60c8407714f848282
mr-nishanth <innocentnishanth@gmail.com>
2023-03-26 08:43:52 +0530

    [A] Request Persistence Storage

--- frontendmasters/browser-storage/coffeemasters-pwa/scripts/app.js
+++ frontendmasters/browser-storage/coffeemasters-pwa/scripts/app.js
@@ -3,6 +3,16 @@ import Order from "./Order.js";
 import Router from "./Router.js";
 
 // Request Persistence Storage
+(async function () {
+  if (navigator.storage && navigator.storage.persist) {
+    if (!(await navigator.storage.persisted())) {
+      const result = await navigator.storage.persist();
+      console.log(`Was Persistent Storage Request granted? ${result}`);
+    } else {
+      console.log(`Persistent Storage already granted`);
+    }
+  }
+})();
 
 window.addEventListener("DOMContentLoaded", () => {
   Router.init();
