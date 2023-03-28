597df6a0a9ce8bc52896c55e5591906b66495479
mr-nishanth <innocentnishanth@gmail.com>
2023-03-28 13:01:59 +0530

    [A] Register and Login

--- frontendmasters/FullStack-Authentication/coffeemasters-authn/server.js
+++ frontendmasters/FullStack-Authentication/coffeemasters-authn/server.js
@@ -1,42 +1,91 @@
-import express from 'express'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-import * as url from 'url';
-import bcrypt from 'bcryptjs';
-import * as jwtJsDecode from 'jwt-js-decode';
+import express from "express";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import * as url from "url";
+import bcrypt from "bcryptjs";
+import * as jwtJsDecode from "jwt-js-decode";
 import base64url from "base64url";
-import SimpleWebAuthnServer from '@simplewebauthn/server';
+import SimpleWebAuthnServer from "@simplewebauthn/server";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const adapter = new JSONFile(__dirname + '/auth.json');
+const adapter = new JSONFile(__dirname + "/auth.json");
 const db = new Low(adapter);
 await db.read();
-db.data ||= { users: [] }
+db.data ||= { users: [] };
 
 const rpID = "localhost";
 const protocol = "http";
 const port = 5050;
 const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// =================================================================
+function findUser(email) {
+  const results = db.data.users.filter((user) => user.email === email);
+  if (results.length === 0) return undefined;
+  return results[0];
+}
 
 // ADD HERE THE REST OF THE ENDPOINTS
+app.post("/auth/register", (req, res) => {
+  // TODO: Data Validation
 
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPass = bcrypt.hashSync(req.body.password, salt);
 
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPass,
+  };
+
+  const userFound = findUser(user.email);
+  if (userFound) {
+    // User already registered
+    res.send({ ok: false, message: "User already registered" });
+  } else {
+    // New User
+    db.data.users.push(user);
+    db.write();
+    res.send({ ok: true });
+  }
+});
+
+app.post("/auth/login", (req, res) => {
+  const userFound = findUser(req.body.email);
+  if (userFound) {
+    // Check Password
+    if (bcrypt.compareSync(req.body.password, userFound.password)) {
+      res.send({
+        ok: true,
+        message: "Login Successfully",
+        name: userFound.name,
+        email: userFound.email,
+      });
+    } else {
+      res.send({ ok: false, message: "Credentials Invalid" });
+    }
+  } else {
+    // User not found
+    res.send({ ok: false, message: "Credentials Invalid" });
+  }
+});
 
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "public/index.html"); 
+  res.sendFile(__dirname + "public/index.html");
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
-
