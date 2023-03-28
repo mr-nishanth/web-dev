1fca84646575a525fc34d22028a7c53c5314f767
mr-nishanth <innocentnishanth@gmail.com>
2023-03-28 13:07:13 +0530

    [A] Login and Register Route

--- frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/API.js
+++ frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/API.js
@@ -1,17 +1,22 @@
 const API = {
-    endpoint: "/auth/",
-    // ADD HERE ALL THE OTHER API FUNCTIONS
-    makePostRequest: async (url, data) => {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        return await response.json();
-    }
-
-}
+  endpoint: "/auth/",
+  // ADD HERE ALL THE OTHER API FUNCTIONS
+  register: async (user) => {
+    return await API.makePostRequest(API.endpoint + "register", user);
+  },
+  login: async (user) => {
+    return await API.makePostRequest(API.endpoint + "login", user);
+  },
+  makePostRequest: async (url, data) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  },
+};
 
 export default API;
