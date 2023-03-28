bd783f6f33bc8c911a0d5a4f8b8d73d57710a6e5
mr-nishanth <innocentnishanth@gmail.com>
2023-03-28 15:34:22 +0530

    [A] Login and Register

--- frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/Auth.js
+++ frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/Auth.js
@@ -2,37 +2,54 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
+  isLoggedIn: false,
+  account: null,
 
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
+  register: async (event) => {
+    event.preventDefault();
+    const user = {
+      name: document.getElementById("register_name").value,
+      email: document.getElementById("register_email").value,
+      password: document.getElementById("register_password").value,
+    };
+    const response = await API.register(user);
+    console.log({ response });
+  },
+  login: async (event) => {
+    event.preventDefault();
+    const user = {
+      email: document.getElementById("login_email").value,
+      password: document.getElementById("login_password").value,
+    };
+    const response = await API.login(user);
+    console.log({ response });
+  },
 
-        }
-    },    
-    init: () => {
-        
-    },
-}
+  updateStatus() {
+    if (Auth.isLoggedIn && Auth.account) {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "none"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".account_name")
+        .forEach((e) => (e.innerHTML = Auth.account.name));
+      document
+        .querySelectorAll(".account_username")
+        .forEach((e) => (e.innerHTML = Auth.account.email));
+    } else {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "none"));
+    }
+  },
+  init: () => {},
+};
 Auth.updateStatus();
 
 export default Auth;
