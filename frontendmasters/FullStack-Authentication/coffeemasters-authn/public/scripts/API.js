094444a4aa32e315fa25f3aecc99d7fdaaaa8a22
mr-nishanth <innocentnishanth@gmail.com>
2023-03-29 10:12:34 +0530

    [A] webAuthn

--- frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/API.js
+++ frontendmasters/FullStack-Authentication/coffeemasters-authn/public/scripts/API.js
@@ -13,6 +13,37 @@ const API = {
   checkAuthOption: async (user) => {
     return await API.makePostRequest(API.endpoint + "auth-options", user);
   },
+  webAuthn: {
+    loginOptions: async (email) => {
+      return await API.makePostRequest(API.endpoint + "webauth-login-options", {
+        email,
+      });
+    },
+    loginVerification: async (email, data) => {
+      return await API.makePostRequest(
+        API.endpoint + "webauth-login-verification",
+        {
+          email,
+          data,
+        }
+      );
+    },
+    registrationOptions: async () => {
+      return await API.makePostRequest(
+        API.endpoint + "webauth-registration-options",
+        Auth.account
+      );
+    },
+    registrationVerification: async (data) => {
+      return await API.makePostRequest(
+        API.endpoint + "webauth-registration-verification",
+        {
+          user: Auth.account,
+          data,
+        }
+      );
+    },
+  },
   makePostRequest: async (url, data) => {
     const response = await fetch(url, {
       method: "POST",
