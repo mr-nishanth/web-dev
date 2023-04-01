8b60ded19d20ca611d1feafe0976a1febcac82fc
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 19:45:44 +0530

    [A] Routes

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/App.js
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/App.js
@@ -1,8 +1,22 @@
 import "./Styles/app.scss";
 import "react-toastify/dist/ReactToastify.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login, Register, Home, Profile, ProfileEdit, Post } from "./pages/";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/profile/:id/edit" element={<ProfileEdit />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
