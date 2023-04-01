086848232076844cdc315d40bf70ee13411b7e8a
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 22:54:42 +0530

    [A] Posts

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Posts.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Posts.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React from 'react';
+import { posts } from '../Constants/dummy';
+import Post from './Post';
+import '../Styles/posts.scss';
 const Posts = () => {
-  return <div>Posts</div>;
+    return (
+        <div className='posts__container'>
+            {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    post={post}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default Posts;
