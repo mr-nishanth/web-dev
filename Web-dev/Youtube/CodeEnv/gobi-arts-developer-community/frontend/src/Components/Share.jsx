0ce0f04f189dcd84f54bc7867dee09e02ae46101
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 22:06:07 +0530

    [A] Basic Share

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Share.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Share.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "../Styles/share.scss";
-
+import { user } from "../Images";
+import { shareOptions } from "../Constants/dummy";
 const Share = () => {
-  return <div>Share</div>;
+  return (
+    <div className="share__container">
+      <div className="share__top">
+        <img src={user} alt={`Mr Nishanth Profile`} draggable={false} />
+        <div className="input__container">
+          <input type="text" placeholder={`What's on your mind?`} />
+        </div>
+      </div>
+      <div className="share__bottom">
+        <ul>
+          {shareOptions.map((shareOption) => (
+            <li key={shareOption}>
+              <shareOption.icon></shareOption.icon>
+            </li>
+          ))}
+        </ul>
+        <button>Post</button>
+      </div>
+    </div>
+  );
 };
 
 export default Share;
