9cc5b20c904c01d531bfc8f649d0d0af50d06aff
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 23:31:08 +0530

    [A] Friends with dummy data

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Friends.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Friends.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import "../Styles/friends.scss";
-
+import React from 'react';
+import '../Styles/friends.scss';
+import { recentRequest } from '../Constants/dummy';
 const Friends = () => {
-  return <div>Friends</div>;
+    return (
+        <div className='friends__container'>
+            {recentRequest.map((request) => (
+                <div
+                    className='friend__container'
+                    key={request?.name}
+                >
+                    <img
+                        src={request?.profile}
+                        alt={`${request?.name} Profile`}
+                    />
+                    <p>
+                        {request?.name} <span>Last seen recently</span>{' '}
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Friends;
