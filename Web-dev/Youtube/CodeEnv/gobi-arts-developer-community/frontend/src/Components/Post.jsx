bbe0cfaf309bbfc4cafafce8ecc6688f5e9c2b3d
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 22:54:32 +0530

    [A] Post using static data

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Post.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/Post.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React from 'react';
+import {
+    BiBookmark,
+    BiCommentDetail,
+    BiDotsVerticalRounded,
+    BiLike,
+} from 'react-icons/bi';
+import { ImShare } from 'react-icons/im';
+import { Link } from 'react-router-dom';
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({ post }) => {
+    return (
+        <div className='post__content'>
+            <div className='post__top'>
+                <div className='post__header'>
+                    <div className='post__user'>
+                        <img
+                            src={post?.profile}
+                            alt={`Mr Nishanth Profile`}
+                        />
+                        <div className='post__user-infos'>
+                            <p>{post?.username}</p>
+                            <span>12 minutes ago</span>
+                        </div>
+                    </div>
+                    <ul className='post__options'>
+                        <li>
+                            <BiBookmark />
+                        </li>
+                        <li>
+                            <BiDotsVerticalRounded />
+                        </li>
+                    </ul>
+                </div>
+                <p className='post__text'>{post.text}</p>
+            </div>
+            <div className='post__preview'>
+                <Link to={`/post/${post.id}`}>
+                    <img
+                        src={post?.img}
+                        alt={`GADC post`}
+                    />
+                </Link>
+                <ul className='post__actions'>
+                    <li>
+                        <BiLike /> Like <span>30</span>
+                    </li>
+                    <li>
+                        <BiCommentDetail /> Comment <span>30</span>
+                    </li>
+                    <li>
+                        <ImShare /> Share <span>30</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Post;
