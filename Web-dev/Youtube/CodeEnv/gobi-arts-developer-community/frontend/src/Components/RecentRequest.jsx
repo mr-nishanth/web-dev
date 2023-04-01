12d875b263ebd793331e29fe4660140ac3bb1c34
mr-nishanth <innocentnishanth@gmail.com>
2023-04-01 23:15:16 +0530

    [A] RecentRequest

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/RecentRequest.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/Components/RecentRequest.jsx
@@ -1,7 +1,29 @@
-import React from "react";
-
+import React from 'react';
+import '../Styles/recentRequest.scss';
+import { recentRequest } from '../Constants/dummy';
 const RecentRequest = () => {
-  return <div>RecentRequest</div>;
+    return (
+        <div className='recent__requests'>
+            {recentRequest.map((request) => (
+                <div
+                    className='recent__container'
+                    key={request?.name}
+                >
+                    <div className='recent__user'>
+                        <img
+                            src={request?.profile}
+                            alt={`${request?.name} Profile`}
+                        />
+                        <p>{request?.name}</p>
+                    </div>
+                    <div className='request__actions'>
+                        <button>Accept</button>
+                        <button>Decline</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default RecentRequest;
