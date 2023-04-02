c54fc01fc86b5e199049195e08461edf598d5bb4
mr-nishanth <innocentnishanth@gmail.com>
2023-04-02 21:37:36 +0530

    [A] User route

--- Web-dev/Youtube/Hitesh Choudhary/prisma-mongodb/index.js
+++ Web-dev/Youtube/Hitesh Choudhary/prisma-mongodb/index.js
@@ -13,7 +13,15 @@ app.use(helmet());
 app.use(logger('dev'));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {});
+app.use('/api', require('./routes/user.routes'));
+
+// 404 page
+app.all('*', (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Invalid path',
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`);
