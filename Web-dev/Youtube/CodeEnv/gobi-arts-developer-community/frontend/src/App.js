4281e80172b1977742fd5974facfc5dcf502d3e6
mr-nishanth <innocentnishanth@gmail.com>
2023-04-02 13:24:51 +0530

    [A] Theme

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/App.js
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/App.js
@@ -1,22 +1,45 @@
-import "./Styles/app.scss";
-import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login, Register, Home, Profile, ProfileEdit, Post } from "./pages/";
+import './Styles/app.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login, Register, Home, Profile, ProfileEdit, Post } from './pages/';
+import { useSelector } from 'react-redux';
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/profile/:id/edit" element={<ProfileEdit />} />
-          <Route path="/post/:id" element={<Post />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const theme = useSelector((state) => state.theme);
+    return (
+        <div
+            className='App'
+            data-theme={theme}
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Home />}
+                    />
+                    <Route
+                        path='/login'
+                        element={<Login />}
+                    />
+                    <Route
+                        path='/register'
+                        element={<Register />}
+                    />
+                    <Route
+                        path='/profile/:id'
+                        element={<Profile />}
+                    />
+                    <Route
+                        path='/profile/:id/edit'
+                        element={<ProfileEdit />}
+                    />
+                    <Route
+                        path='/post/:id'
+                        element={<Post />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
