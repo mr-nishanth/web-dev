1b4361cf9b93e984a29dfa011c1ca7b25d2f00c3
mr-nishanth <innocentnishanth@gmail.com>
2023-04-02 11:47:13 +0530

    [A] Basic ProfileEdit JSX

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/pages/ProfileEdit.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/pages/ProfileEdit.jsx
@@ -1,8 +1,65 @@
-import React from "react";
-import "../Styles/profileEdit.scss";
+import React from 'react';
+import '../Styles/profileEdit.scss';
+import { BiPencil } from 'react-icons/bi';
+import { Sidebar } from '../Components';
+import { cover, user } from '../Images';
 
 const ProfileEdit = () => {
-  return <div>ProfileEdit</div>;
+    const handleEditUser = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className='app__profileEdit'>
+            <Sidebar />
+            <div className='profileEdit__container'>
+                <form onSubmit={handleEditUser}>
+                    <div className='user__profile__picture'>
+                        <div className='cover__edit'>
+                            <img
+                                src={cover}
+                                alt={`Mr Nishanth Cover`}
+                                className='userCoverPic'
+                            />
+                            <div className='edit'>
+                                <BiPencil />
+                            </div>
+                        </div>
+                        <div className='profile__picture'>
+                            <img
+                                src={user}
+                                alt={`Mr Nishanth Profile`}
+                                className='userProfile'
+                            />
+                            <div className='edit'>
+                                <BiPencil />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='input__wrapper'>
+                        <div className='input__container'>
+                            <input
+                                type='text'
+                                placeholder='Username....'
+                            />
+                        </div>
+                        <div className='input__container'>
+                            <input
+                                type='email'
+                                placeholder='Email....'
+                            />
+                        </div>
+                        <div className='input__container'>
+                            <input
+                                type='password'
+                                placeholder='Password....'
+                            />
+                        </div>
+                        <button className=''>Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ProfileEdit;
