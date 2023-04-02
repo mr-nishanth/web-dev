9509d86ee94bfc5ffe3ab94d6175bb85d477daf7
mr-nishanth <innocentnishanth@gmail.com>
2023-04-02 09:37:27 +0530

    [A] Basic Register jsx

--- Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/pages/Register.jsx
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/pages/Register.jsx
@@ -1,8 +1,62 @@
-import React from "react";
-import "../Styles/auth.scss";
+import React from 'react';
+import '../Styles/auth.scss';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    const handleGoogleAuth = () => {};
+    const handleGithubAuth = () => {};
+    return (
+        <div className='app__auth register'>
+            <form onSubmit={handleSubmit}>
+                <p className='auth__header'>
+                    Gobi arts Developer community <span>Register</span>
+                </p>
+                <div className='input__container'>
+                    <input
+                        type='text'
+                        placeholder='Username....'
+                    />
+                </div>
+                <div className='input__container'>
+                    <input
+                        type='email'
+                        placeholder='Email....'
+                    />
+                </div>
+                <div className='input__container'>
+                    <input
+                        type='password'
+                        placeholder='Password....'
+                    />
+                </div>
+                <button className='auth__action'>Register</button>
+                <div className='or__container'>
+                    <div className='or__text'>
+                        <p>OR</p>
+                    </div>
+                    <div className='or__wrapper'>
+                        <button
+                            className='google'
+                            onClick={handleGoogleAuth}
+                        >
+                            Google
+                        </button>
+                        <button
+                            className='github'
+                            onClick={handleGithubAuth}
+                        >
+                            Github
+                        </button>
+                    </div>
+                </div>
+                <p className='linkWrapper'>
+                    Already have an account? <Link to={'/login'}>Login</Link>
+                </p>
+            </form>
+        </div>
+    );
 };
-
 export default Register;
