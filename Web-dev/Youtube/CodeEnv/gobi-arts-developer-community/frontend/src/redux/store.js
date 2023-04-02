a198196bf471211b3fcdbc431cf1c3e72a2b76c8
mr-nishanth <innocentnishanth@gmail.com>
2023-04-02 13:25:44 +0530

    [A] configuration store

---
+++ Web-dev/Youtube/CodeEnv/gobi-arts-developer-community/frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import themeReducer from './theme';
+
+export const store = configureStore({
+    reducer: {
+        theme: themeReducer,
+    },
+});
