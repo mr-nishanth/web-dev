e703fd8fb2f780a2697eee682807f21cec90af27
mr-nishanth <innocentnishanth@gmail.com>
2023-06-11 08:18:07 +0530

    [A] Configuration

--- Web-dev/MegaCourses/CodeWithMosh/Backend_Nodejs/index.js
+++ Web-dev/MegaCourses/CodeWithMosh/Backend_Nodejs/index.js
@@ -1,13 +1,36 @@
 require('dotenv').config();
+const config = require('config');
 const express = require('express');
 const Joi = require('joi');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json()); // req.body
+app.use(express.urlencoded({ extended: true })); // req.body [key1=value1&ke2y=value2]
+app.use(express.static('public'));
+app.use(helmet());
+
+// Configuration
+if (
+    config.has('name') ||
+    config.has('mail.host') ||
+    config.has('mail.app_password') ||
+    config.has('mail.mail_password')
+) {
+    console.log(`Environment : ${config.util.getEnv('NODE_ENV')}`);
+
+    console.log(`Application Name : ${config.get('name')}`);
+    console.log(`App Password : ${config.get('mail.app_password')}`);
+    console.log(`Mail Server : ${config.get('mail.host')}`);
+    console.log(`Mail Server Password : ${config.get('mail.mail_password')}`);
+}
+
+if (app.get('env') === 'development') {
+    app.use(morgan('dev'));
+}
 
 const port = process.env.PORT ?? 3500;
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
