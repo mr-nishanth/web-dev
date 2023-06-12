f4ef0e7d3036ee499d983f3cbc19991c4afa67dd
mr-nishanth <innocentnishanth@gmail.com>
2023-06-12 17:43:41 +0530

    [A] Post

--- Web-dev/Youtube/IndianCoders/NextBlog/full-stack/app/api/blog/route.ts
+++ Web-dev/Youtube/IndianCoders/NextBlog/full-stack/app/api/blog/route.ts
@@ -2,7 +2,7 @@ import prisma from '@/prisma';
 import { NextResponse } from 'next/server';
 
 // Connection functions
-async function main() {
+export async function main() {
     try {
         await prisma.$connect();
     } catch (error) {
@@ -16,7 +16,7 @@ export const GET = async (req: Request, res: NextResponse) => {
         await main();
         const posts = await prisma.post.findMany();
         return NextResponse.json(
-            { message: 'Success', posts },
+            { message: 'Success', counts: posts.length, posts },
             { status: 200 }
         );
     } catch (error) {
@@ -28,4 +28,17 @@ export const GET = async (req: Request, res: NextResponse) => {
 
 export const POST = async (req: Request, res: NextResponse) => {
     console.log('POST request');
+    try {
+        const { title, description } = await req.json();
+
+        await main();
+        const post = await prisma.post.create({
+            data: { title: title, description: description },
+        });
+        return NextResponse.json({ message: 'Success', post }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Error', error }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
 };
