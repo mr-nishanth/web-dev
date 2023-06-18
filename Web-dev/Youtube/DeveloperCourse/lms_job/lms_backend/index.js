6d3a464fe1dadd792c3b67aa4756f718a5575941
mr-nishanth <innocentnishanth@gmail.com>
2023-06-18 10:13:17 +0530

    [A] Initial setup

---
+++ Web-dev/Youtube/DeveloperCourse/lms_job/lms_backend/index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const dbConnect = require('./config/dbConnection');
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+app.listen(PORT, async () => {
+    console.log(`Server listening on port : http://localhost:${PORT}`);
+    await dbConnect();
+});
