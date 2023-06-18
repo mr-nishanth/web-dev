6eca323621758f59309a92828f7dbf0675a0959b
mr-nishanth <innocentnishanth@gmail.com>
2023-06-18 11:32:13 +0530

    [A] User route

--- Web-dev/Youtube/DeveloperCourse/lms_job/lms_backend/index.js
+++ Web-dev/Youtube/DeveloperCourse/lms_job/lms_backend/index.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const dbConnect = require('./configs/dbConnection');
+const { notFound, handleError } = require('./middlewares/errorHandler');
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/user', require('./routes/user.route'));
+
+app.use(notFound);
+app.use(handleError);
 app.listen(PORT, async () => {
     console.log(`Server listening on port : http://localhost:${PORT}`);
     await dbConnect();
