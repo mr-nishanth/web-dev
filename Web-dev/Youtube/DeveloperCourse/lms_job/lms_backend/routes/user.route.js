ea3d110fa108f1ba6b3acafa5386241481dbcb9a
mr-nishanth <innocentnishanth@gmail.com>
2023-06-26 11:22:54 +0530

    [A] login ,update and all users

--- Web-dev/Youtube/DeveloperCourse/lms_job/lms_backend/routes/user.route.js
+++ Web-dev/Youtube/DeveloperCourse/lms_job/lms_backend/routes/user.route.js
@@ -1,10 +1,22 @@
 // const router = require('express').Router();
 // module.exports = router;
 
-const { registerUser } = require('../controllers/user.controller');
+const authMiddleware = require('../controllers/authMiddleware');
+const { isAdmin, isInstructor } = require('../controllers/rolesMiddleware');
+const {
+    registerUser,
+    loginUser,
+    getAllUsers,
+    updateUser,
+} = require('../controllers/user.controller');
 
 const router = require('express').Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.put('/update', [authMiddleware], updateUser);
+
+router.get('/all-users', [authMiddleware, isInstructor, isAdmin], getAllUsers);
 
 module.exports = router;
