4403fbf0a4c7ceee14f93965e67bc7aeff4b6351
mr-nishanth <innocentnishanth@gmail.com>
2023-07-14 07:35:20 +0530

    [A] Feed Page

---
+++ Web-dev/July-10/React/Dennis Lvy/threads/src/pages/Feed.jsx
@@ -0,0 +1,19 @@
+const Feed = () => {
+    return (
+        <div className='container mx-auto w-[600px] bg-slate-400'>
+            <div className=''>
+                <img
+                    src='https://avatars.githubusercontent.com/u/67851976?v=4'
+                    alt='My Profile'
+                />
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feed;
