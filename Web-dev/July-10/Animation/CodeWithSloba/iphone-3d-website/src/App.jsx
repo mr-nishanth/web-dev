cf89c4f454dae4594ae1ac8801f16dc33c287b12
mr-nishanth <innocentnishanth@gmail.com>
2023-07-15 08:38:35 +0530

    [A] WebgiViewer

--- Web-dev/July-10/Animation/CodeWithSloba/iphone-3d-website/src/App.jsx
+++ Web-dev/July-10/Animation/CodeWithSloba/iphone-3d-website/src/App.jsx
@@ -1,11 +1,19 @@
+import DisplaySection from './components/DisplaySection';
+import Jumbotron from './components/Jumbotron';
+import Nav from './components/Nav';
+import SoundSection from './components/SoundSection';
+import WebgiViewer from './components/WebgiViewer';
 
 function App() {
-
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Nav />
+            <Jumbotron />
+            <SoundSection />
+            <DisplaySection />
+            <WebgiViewer />
+        </div>
+    );
 }
 
 export default App;
