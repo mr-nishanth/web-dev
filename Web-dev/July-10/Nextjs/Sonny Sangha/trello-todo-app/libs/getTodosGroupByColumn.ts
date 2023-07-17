2e2eb27ab628ef7aed727a7322f9cf338d2b3eae
mr-nishanth <innocentnishanth@gmail.com>
2023-07-17 21:50:08 +0530

    [A] getTodosGroupByColumn

---
+++ Web-dev/July-10/Nextjs/Sonny Sangha/trello-todo-app/libs/getTodosGroupByColumn.ts
@@ -0,0 +1,56 @@
+import { databases } from '@/appwrite';
+import { Board, Column, TypedColumn } from '@/typing';
+
+export const getTodosGroupByColumn = async () => {
+    // Pull the information from database
+
+    const data = await databases.listDocuments(
+        process.env.NEXT_PUBLIC_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+    );
+
+    const todos = data.documents;
+
+    const columns = todos.reduce((acc, todo) => {
+        if (!acc.get(todo.status)) {
+            acc.set(todo.status, { id: todo.status, todos: [] });
+        }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id,
+            $createdAt: todo.$createdAt,
+            title: todo.title,
+            status: todo.status,
+            // Get the image if it exists on the todo
+            ...(todo.image && { image: JSON.parse(todo.image) }),
+        });
+
+        return acc;
+    }, new Map<TypedColumn, Column>());
+
+    console.log({ columns });
+
+    // if the column doesn't have inprogress,todo and done,add then with empty todos
+    const columnTypes: TypedColumn[] = ['todo', 'inprogress', 'done'];
+
+    for (const columnType of columnTypes) {
+        if (!columns.get(columnType)) {
+            columns.set(columnType, {
+                id: columnType,
+                todos: [],
+            });
+        }
+    }
+
+    // sort columns by columnType
+    const sortedColumns = new Map(
+        Array.from(columns.entries()).sort(
+            (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+        )
+    );
+
+    const board: Board = {
+        columns: sortedColumns,
+    };
+    return board;
+};
