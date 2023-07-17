848cba21c4452e59b848eed27a92187fa37bb4e9
mr-nishanth <innocentnishanth@gmail.com>
2023-07-17 16:00:22 +0530

    [A] Initial code

---
+++ Web-dev/July-10/Nextjs/Sonny Sangha/trello-todo-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+
+export default function Home() {
+    return (
+        <main>
+            {/* Header */}
+            <Header />
+            {/* Board */}
+        </main>
+    );
+}
