594c8567687c1a6b81df6317dce727665e0a8168
mr-nishanth <innocentnishanth@gmail.com>
2023-07-18 17:27:34 +0530

    [A] Modal

--- Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/components/Column.tsx
+++ Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/components/Column.tsx
@@ -2,6 +2,7 @@ import { PlusCircleIcon } from '@heroicons/react/24/solid';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import TodoCard from './TodoCard';
 import { useBoardStore } from '@/store/BoardStore';
+import { useModalStore } from '@/store/ModalStore';
 
 type Props = {
     id: TypedColumn;
@@ -19,6 +20,7 @@ const idToColumnText: {
 
 function Column({ id, todos, index }: Props) {
     const [searchString] = useBoardStore((state) => [state.searchString]);
+    const openModal = useModalStore((state) => state.openModal);
     return (
         <Draggable
             draggableId={id}
@@ -100,8 +102,11 @@ function Column({ id, todos, index }: Props) {
                                     {provided.placeholder}
                                     {/* Button [Modal] */}
                                     <div className='flex items-end justify-end p-2'>
-                                        <button className='text-green-500 hover:text-green-600'>
-                                            <PlusCircleIcon className='w-10 h-10' />{' '}
+                                        <button
+                                            onClick={openModal}
+                                            className='text-green-500 hover:text-green-600'
+                                        >
+                                            <PlusCircleIcon className='w-10 h-10' />
                                         </button>
                                     </div>
                                 </div>
