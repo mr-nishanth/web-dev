7562d5ee585afade514dd6a8963fc190e3c0a74e
mr-nishanth <innocentnishanth@gmail.com>
2023-07-18 10:10:50 +0530

    [A] Search and UpdateDB feature

--- Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/store/BoardStore.ts
+++ Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/store/BoardStore.ts
@@ -1,3 +1,4 @@
+import { databases } from '@/appwrite';
 import { getTodosGroupByColumn } from '@/utils/getTodosGroupByColumn';
 import { create } from 'zustand';
 
@@ -5,16 +6,36 @@ interface BoardStore {
     board: Board;
     getBoard: () => Promise<void>;
     setBoardState: (board: Board) => void;
+    updateTodoInDB: (todo: Todo, columnId: TypedColumn) => Promise<void>;
+
+    searchString: string;
+    setSearchString: (searchString: string) => void;
 }
 
 export const useBoardStore = create<BoardStore>((set) => ({
     board: {
         columns: new Map<TypedColumn, Column>(),
     },
+
     getBoard: async () => {
         const board = await getTodosGroupByColumn();
         set({ board });
     },
 
     setBoardState: (board) => set({ board }),
+
+    updateTodoInDB: async (todo, columnId) => {
+        await databases.updateDocument(
+            process.env.NEXT_PUBLIC_DATABASE_ID!,
+            process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+            todo.$id,
+            {
+                title: todo.title,
+                status: columnId,
+            }
+        );
+    },
+
+    searchString: '',
+    setSearchString: (searchString) => set({ searchString }),
 }));
