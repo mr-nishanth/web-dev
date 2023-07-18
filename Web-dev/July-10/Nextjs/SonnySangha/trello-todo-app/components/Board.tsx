45486cfebd279e98cf5cfcba80356493b4cfa589
mr-nishanth <innocentnishanth@gmail.com>
2023-07-18 10:09:43 +0530

    [A] Drag and Drop Feature

--- Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/components/Board.tsx
+++ Web-dev/July-10/Nextjs/SonnySangha/trello-todo-app/components/Board.tsx
@@ -1,4 +1,8 @@
 'use client';
+
+// TODO:
+// Figure out the error while dragging the column
+
 import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
@@ -6,11 +10,14 @@ import Column from './Column';
 
 function Board() {
     // const getBoard = useBoardStore((state) => state.getBoard);
-    const [board, getBoard, setBoardState] = useBoardStore((state) => [
-        state.board,
-        state.getBoard,
-        state.setBoardState,
-    ]);
+    const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+        (state) => [
+            state.board,
+            state.getBoard,
+            state.setBoardState,
+            state.updateTodoInDB,
+        ]
+    );
 
     useEffect(() => {
         getBoard();
@@ -30,6 +37,70 @@ function Board() {
             entries.splice(destination.index, 0, removed);
             const rearrangedColumns = new Map(entries);
             setBoardState({ ...board, columns: rearrangedColumns });
+        }
+
+        // This step is need as the indexes are stored as numbers 0,1,2 etc.Instead of id's with DND library
+        //1. create copy of the column
+        const columns = Array.from(board.columns);
+        const startColumnIndex = columns[Number(source.droppableId)];
+        const finishColumnIndex = columns[Number(destination.droppableId)];
+        console.log({ startColumnIndex, finishColumnIndex });
+
+        // Rebuild start and finish columns
+        const startCol: Column = {
+            id: startColumnIndex[0],
+            todos: startColumnIndex[1].todos,
+        };
+
+        const finishCol: Column = {
+            id: finishColumnIndex[0],
+            todos: finishColumnIndex[1].todos,
+        };
+        console.log({ startCol, finishCol });
+
+        if (!startCol || !finishCol) return;
+
+        // Same column with same index
+        if (source.index === destination.index && startCol === finishCol)
+            return;
+
+        const newTodos = startCol.todos;
+        const [todoMoved] = newTodos.splice(source.index, 1);
+
+        // Dragging and Dropping same column
+        if (startCol.id === finishCol.id) {
+            // Same column task drag
+            newTodos.splice(destination.index, 0, todoMoved);
+            const newCol = {
+                id: startCol.id,
+                todos: newTodos,
+            };
+            // Create copy of board
+            const newColumns = new Map(board.columns);
+            newColumns.set(startCol.id, newCol);
+            setBoardState({ ...board, columns: newColumns });
+        } else {
+            // Dragging to another column
+
+            // Make a copy of finishColumn
+            const finishTodos = Array.from(finishCol.todos);
+            finishTodos.splice(destination.index, 0, todoMoved);
+            const newCol = {
+                id: startCol.id,
+                todos: newTodos,
+            };
+            // Create copy of board
+            const newColumns = new Map(board.columns);
+
+            newColumns.set(startCol.id, newCol);
+            newColumns.set(finishCol.id, {
+                id: finishCol.id,
+                todos: finishTodos,
+            });
+
+            // Update in DB
+            updateTodoInDB(todoMoved, finishCol.id);
+            setBoardState({ ...board, columns: newColumns });
         }
     };
 
