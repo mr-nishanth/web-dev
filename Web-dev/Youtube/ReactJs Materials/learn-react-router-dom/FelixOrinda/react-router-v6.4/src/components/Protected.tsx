90a4be343b657fd178282c52d2caf6498b85fe0c
mr-nishanth <innocentnishanth@gmail.com>
2023-07-22 22:43:39 +0530

    [U] Code Style

--- Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/components/Protected.tsx
+++ Web-dev/Youtube/ReactJs Materials/learn-react-router-dom/FelixOrinda/react-router-v6.4/src/components/Protected.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { useState } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 
 type Props = {
-  children: JSX.Element;
+    children: JSX.Element;
 };
 
 const Protected = ({ children }: Props) => {
-  const [isAuth, setIsAuth] = useState<boolean>(false);
-  const location = useLocation().pathname;
+    const [isAuth, setIsAuth] = useState<boolean>(false);
+    const location = useLocation().pathname;
 
-  return isAuth ? (
-    <>{children}</>
-  ) : (
-    <Navigate to={`/`} state={{ from: location }} replace />
-  );
+    return isAuth ? (
+        <>{children}</>
+    ) : (
+        <Navigate
+            to={`/`}
+            state={{ from: location }}
+            replace
+        />
+    );
 };
 export default Protected;
